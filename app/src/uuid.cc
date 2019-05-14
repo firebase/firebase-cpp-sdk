a51ae8f81b8baae8af52d61973adc555aa2c0a64
smiles <smiles@google.com>
2019-05-14 14:00:55 -0700

    Desktop UUID generator class.
    Windows adds the requirement that apps link Rpcrt4.lib which isn't
    present in third_party/msvc at the moment so the test won't link.
    
    Added disabled code for Linux that can be enabled to use libuuid if/when
    we add it to third_party.  For the moment Linux uses /dev/urandom to generate
    a random 128-bit number.
    
    iOS / Darwin uses NSUUID which appears to work fine.
    
    Android is not required at the moment and therefore not supported.
    
    PiperOrigin-RevId: 248206226

---
+++ app/src/uuid.cc
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#if defined(_WIN32)
+#include <combaseapi.h>
+#elif HAVE_LIBUUID
+#include <uuid/uuid.h>
+#else
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#endif  // defined(_WIN32)
+
+#include "app/src/assert.h"
+#include "app/src/uuid.h"
+
+namespace firebase {
+namespace internal {
+
+void Uuid::Generate() {
+#if defined(_WIN32)
+  FIREBASE_ASSERT(sizeof(GUID) == sizeof(data));
+  HRESULT result = CoCreateGuid(reinterpret_cast<GUID*>(data));
+  FIREBASE_ASSERT(result == S_OK);
+  (void)result;
+#elif HAVE_LIBUUID
+  static_assert(sizeof(uuid_t) == sizeof(data));
+  uuid_generate_time(reinterpret_cast<uuid_t>(data));
+#else
+  int file = open("/dev/urandom", O_RDONLY);
+  FIREBASE_ASSERT(file >= 0);
+  size_t bytes_read = read(file, data, sizeof(data));
+  FIREBASE_ASSERT(bytes_read == sizeof(data));
+  close(file);
+#endif  // defined(_WIN32)
+}
+
+}  // namespace internal
+}  // namespace firebase
