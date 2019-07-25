ef54ed2c08cda7f609532d501ddba1e7f034a2aa
amaurice <amaurice@google.com>
2019-07-25 16:18:44 -0700

    Seperate the CMake cc_test based on target platform
    Most of the tests are platform specific, so defining them as cc_test, cc_test_on_ios, etc, instead of all in cc_test is a more sensible approach.
    
    PiperOrigin-RevId: 260042373

--- cmake/test_rules.cmake
+++ cmake/test_rules.cmake
@@ -23,34 +23,56 @@ include(CMakeParseArguments)
 # Defines a new test executable target with the given target name, sources, and
 # dependencies.  Implicitly adds DEPENDS on gtest and gtest_main.
 function(cc_test name)
-  set(multi DEPENDS SOURCES INCLUDES DEFINES
-      ANDROID_DEPENDS ANDROID_SOURCES ANDROID_INCLUDES ANDROID_DEFINES
-      IOS_DEPENDS IOS_SOURCES IOS_INCLUDES IOS_DEFINES
-      DESKTOP_DEPENDS DESKTOP_SOURCES DESKTOP_INCLUDES DESKTOP_DEFINES)
+  if (ANDROID OR IOS)
+    return()
+  endif()
+
+  set(multi DEPENDS SOURCES INCLUDES DEFINES)
   # Parse the arguments into cc_test_SOURCES and cc_test_DEPENDS.
   cmake_parse_arguments(cc_test "" "" "${multi}" ${ARGN})
 
   list(APPEND cc_test_DEPENDS gmock gtest gtest_main)
-  if (ANDROID)
-    list(APPEND cc_test_SOURCES "${cc_test_ANDROID_SOURCES}")
-    list(APPEND cc_test_DEPENDS "${cc_test_ANDROID_DEPENDS}")
-    list(APPEND cc_test_INCLUDES "${cc_test_ANDROID_INCLUDES}")
-    list(APPEND cc_test_DEFINES "${cc_test_ANDROID_DEFINES}")
-  elseif (IOS)
-    list(APPEND cc_test_SOURCES "${cc_test_IOS_SOURCES}")
-    list(APPEND cc_test_DEPENDS
-         "${cc_test_IOS_DEPENDS}"
-         "-framework CoreFoundation"
-         "-framework Foundation"
-    )
-    list(APPEND cc_test_INCLUDES "${cc_test_IOS_INCLUDES}")
-    list(APPEND cc_test_DEFINES "${cc_test_IOS_DEFINES}")
-  else()
-    list(APPEND cc_test_SOURCES "${cc_test_DESKTOP_SOURCES}")
-    list(APPEND cc_test_DEPENDS "${cc_test_DESKTOP_DEPENDS}")
-    list(APPEND cc_test_INCLUDES "${cc_test_DESKTOP_INCLUDES}")
-    list(APPEND cc_test_DEFINES "${cc_test_DESKTOP_DEFINES}")
+
+  add_executable(${name} ${cc_test_SOURCES})
+  add_test(${name} ${name})
+  target_include_directories(${name}
+    PRIVATE
+      ${FIREBASE_SOURCE_DIR}
+      ${cc_test_INCLUDES}
+  )
+  target_link_libraries(${name} PRIVATE ${cc_test_DEPENDS})
+  target_compile_definitions(${name}
+    PRIVATE
+      -DINTERNAL_EXPERIMENTAL=1
+      ${cc_test_DEFINES}
+  )
+endfunction()
+
+# cc_test_on_ios(
+#   target
+#   SOURCES sources...
+#   DEPENDS libraries...
+# )
+#
+# Defines a new test executable target with the given target name, sources, and
+# dependencies.  Implicitly adds DEPENDS on gtest, gtest_main, Foundation and
+# CoreFoundation frameworks.
+function(cc_test_on_ios name)
+  if (NOT IOS)
+    return()
   endif()
+
+  set(multi DEPENDS SOURCES INCLUDES DEFINES)
+  # Parse the arguments into cc_test_SOURCES and cc_test_DEPENDS.
+  cmake_parse_arguments(cc_test "" "" "${multi}" ${ARGN})
+
+  list(APPEND cc_test_DEPENDS
+       gmock
+       gtest
+       gtest_main
+       "-framework CoreFoundation"
+       "-framework Foundation"
+  )
 
   add_executable(${name} ${cc_test_SOURCES})
   add_test(${name} ${name})
