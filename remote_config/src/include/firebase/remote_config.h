ce8d881cfc93fa86fa950379abba7d5d0b4451f5
cynthiajiang <cynthiajiang@google.com>
2019-12-10 15:38:53 -0800

    [RemoteConfig]V2 - CPP API change for API review
    go/new-rc-cpp-unity-design
    go/new-rc-cpp-unity-api
    
    PiperOrigin-RevId: 284864568

--- remote_config/src/include/firebase/remote_config.h
+++ remote_config/src/include/firebase/remote_config.h
@@ -155,6 +155,25 @@ struct ConfigKeyValueVariant {
 };
 #endif  // SWIG
 
+#ifdef FIREBASE_EARLY_ACCESS_PREVIEW
+/// @brief Configurations for Remote Config behavior.
+struct ConfigSettings {
+  /// The timeout specifies how long the client should wait for a connection to
+  /// the Firebase Remote Config servers.
+  ///
+  /// @note A fetch call will fail if it takes longer than the specified timeout
+  /// to connect to the Remote Config servers. Default is 60 seconds.
+  uint64_t fetch_timeout_in_milliseconds;
+
+  /// The minimum interval between successive fetch calls.
+  ///
+  /// @note Fetches less than duration seconds after the last fetch from the
+  /// Firebase Remote Config server would use values returned during the last
+  /// fetch. Default is 12 hours.
+  uint64_t minimum_fetch_interval_in_milliseconds;
+};
+#endif  // FIREBASE_EARLY_ACCESS_PREVIEW
+
 /// @brief Initialize the RemoteConfig API.
 ///
 /// This must be called prior to calling any other methods in the
@@ -245,7 +264,7 @@ void SetConfigSetting(ConfigSetting setting, const char* value);
 ///
 /// @param[in] key Key of the value to be retrieved.
 ///
-/// @returns Value associated with the specified key converted to a boolean
+/// @return Value associated with the specified key converted to a boolean
 /// value.
 bool GetBoolean(const char* key);
 
@@ -259,7 +278,7 @@ bool GetBoolean(const char* key);
 /// @param[out] info A return value, specifying the source of the returned
 /// value.
 ///
-/// @returns Value associated with the specified key converted to a boolean
+/// @return Value associated with the specified key converted to a boolean
 /// value.
 bool GetBoolean(const char* key, ValueInfo* info);
 
@@ -268,7 +287,7 @@ bool GetBoolean(const char* key, ValueInfo* info);
 ///
 /// @param[in] key Key of the value to be retrieved.
 ///
-/// @returns Value associated with the specified key converted to a 64-bit
+/// @return Value associated with the specified key converted to a 64-bit
 /// integer.
 int64_t GetLong(const char* key);
 
@@ -279,7 +298,7 @@ int64_t GetLong(const char* key);
 /// @param[out] info A return value, specifying the source of the returned
 /// value.
 ///
-/// @returns Value associated with the specified key converted to a 64-bit
+/// @return Value associated with the specified key converted to a 64-bit
 /// integer.
 int64_t GetLong(const char* key, ValueInfo* info);
 
@@ -287,7 +306,7 @@ int64_t GetLong(const char* key, ValueInfo* info);
 ///
 /// @param[in] key Key of the value to be retrieved.
 ///
-/// @returns Value associated with the specified key converted to a double.
+/// @return Value associated with the specified key converted to a double.
 double GetDouble(const char* key);
 
 /// @brief Returns the value associated with a key, converted to a double.
@@ -296,7 +315,7 @@ double GetDouble(const char* key);
 /// @param[out] info A return value, specifying the source of the returned
 /// value.
 ///
-/// @returns Value associated with the specified key converted to a double.
+/// @return Value associated with the specified key converted to a double.
 double GetDouble(const char* key, ValueInfo* info);
 
 /// @brief Returns the value associated with a key, converted to a string.
@@ -320,7 +339,7 @@ std::string GetString(const char* key, ValueInfo* info);
 ///
 /// @param[in] key Key of the value to be retrieved.
 ///
-/// @returns Vector of bytes.
+/// @return Vector of bytes.
 std::vector<unsigned char> GetData(const char* key);
 
 /// @brief Returns the value associated with a key, as a vector of raw
@@ -330,7 +349,7 @@ std::vector<unsigned char> GetData(const char* key);
 /// @param[out] info A return value, specifying the source of the returned
 /// value.
 ///
-/// @returns Vector of bytes.
+/// @return Vector of bytes.
 std::vector<unsigned char> GetData(const char* key, ValueInfo* info);
 
 /// @brief Gets the set of keys that start with the given prefix.
@@ -338,14 +357,14 @@ std::vector<unsigned char> GetData(const char* key, ValueInfo* info);
 /// @param[in] prefix The key prefix to look for. If empty or null, this
 /// method will return all keys.
 ///
-/// @returns Set of Remote Config parameter keys that start with the specified
+/// @return Set of Remote Config parameter keys that start with the specified
 /// prefix. Will return an empty set if there are no keys with the given
 /// prefix.
 std::vector<std::string> GetKeysByPrefix(const char* prefix);
 
 /// @brief Gets the set of all keys.
 ///
-/// @returns Set of all Remote Config parameter keys.
+/// @return Set of all Remote Config parameter keys.
 std::vector<std::string> GetKeys();
 
 /// @brief Fetches config data from the server.
@@ -356,7 +375,7 @@ std::vector<std::string> GetKeys();
 /// Note that this function is asynchronous, and will normally take an
 /// unspecified amount of time before completion.
 ///
-/// @returns A Future which can be used to determine when the fetch is
+/// @return A Future which can be used to determine when the fetch is
 /// complete.
 Future<void> Fetch();
 
@@ -374,7 +393,7 @@ Future<void> Fetch();
 /// and does not fetch any data. A cache_expiration_in_seconds of zero will
 /// always cause a fetch.
 ///
-/// @returns A Future which can be used to determine when the fetch is
+/// @return A Future which can be used to determine when the fetch is
 /// complete.
 Future<void> Fetch(uint64_t cache_expiration_in_seconds);
 
@@ -392,7 +411,7 @@ Future<void> FetchLastResult();
 /// until @ref ActivateFetched() is called.  This gives the developer control
 /// over when newly fetched data is visible to their application.
 ///
-/// @returns true if a previously fetch configuration was activated, false
+/// @return true if a previously fetch configuration was activated, false
 /// if a fetched configuration wasn't found or the configuration was previously
 /// activated.
 bool ActivateFetched();
@@ -400,9 +419,334 @@ bool ActivateFetched();
 /// @brief Returns information about the last fetch request, in the form
 /// of a ConfigInfo struct.
 ///
-/// @returns A ConfigInfo struct, containing fields reflecting the state
+/// @return A ConfigInfo struct, containing fields reflecting the state
 /// of the most recent fetch request.
 const ConfigInfo& GetInfo();
+
+#ifdef FIREBASE_EARLY_ACCESS_PREVIEW
+
+#ifndef SWIG
+/// @brief Entry point for the Firebase C++ SDK for Remote Config.
+///
+/// To use the SDK, call firebase::remote_config::RemoteConfig::GetInstance() to
+/// obtain an instance of RemoteConfig, then call operations on that instance.
+/// The instance contains the complete set of FRC parameter values available to
+/// your app. The instance also stores values fetched from the FRC Server until
+/// they are made available for use with a call to @ref Activate().
+#endif  // SWIG
+class RemoteConfig {
+ public:
+  ~RemoteConfig();
+
+  /// @brief Returns a Future that contains ConfigInfo representing the
+  /// initialization status of this Firebase Remote Config instance.
+  /// Use this method to ensure Set/Get call not being blocked.
+  Future<ConfigInfo> EnsureInitialized();
+
+  /// @brief Get the (possibly still pending) results of the most recent
+  /// EnsureInitialized() call.
+  ///
+  /// @return The future result from the last call to EnsureInitialized().
+  Future<ConfigInfo> EnsureInitializedLastResult();
+
+  /// Asynchronously activates the most recently fetched configs, so that the
+  /// fetched key value pairs take effect.
+  ///
+  /// @return A Future that contains true if fetched configs were
+  /// activated. The future will contain false if the configs were already
+  /// activated.
+  Future<bool> Activate();
+
+  /// @brief Get the (possibly still pending) results of the most recent
+  /// Activate() call.
+  ///
+  /// @return The future result from the last call to Activate().
+  Future<bool> ActivateLastResult();
+
+  /// Asynchronously fetches and then activates the fetched configs.
+  ///
+  /// If the time elapsed since the last fetch from the Firebase Remote Config
+  /// backend is more than the default minimum fetch interval, configs are
+  /// fetched from the backend.
+  ///
+  /// After the fetch is complete, the configs are activated so that the fetched
+  /// key value pairs take effect.
+  ///
+  /// @return A Future that contains true if the current call
+  /// activated the fetched configs; if no configs were fetched from the backend
+  /// and the local fetched configs have already been activated, the future will
+  /// contain false.
+  Future<bool> FetchAndActivate();
+
+  /// @brief Get the (possibly still pending) results of the most recent
+  /// FetchAndActivate() call.
+  ///
+  /// @return The future result from the last call to FetchAndActivate().
+  Future<bool> FetchAndActiveLastResult();
+
+  /// @brief Fetches config data from the server.
+  ///
+  /// @note This does not actually apply the data or make it accessible,
+  /// it merely retrieves it and caches it.  To accept and access the newly
+  /// retrieved values, you must call @ref ActivateFetched().
+  /// Note that this function is asynchronous, and will normally take an
+  /// unspecified amount of time before completion.
+  ///
+  /// @return A Future which can be used to determine when the fetch is
+  /// complete.
+  Future<void> Fetch();
+
+  /// @brief Fetches config data from the server.
+  ///
+  /// @note This does not actually apply the data or make it accessible,
+  /// it merely retrieves it and caches it.  To accept and access the newly
+  /// retrieved values, you must call @ref ActivateFetched().
+  /// Note that this function is asynchronous, and will normally take an
+  /// unspecified amount of time before completion.
+  ///
+  /// @param[in] cache_expiration_in_seconds The number of seconds to keep
+  /// previously fetch data available.  If cached data is available that is
+  /// newer than cache_expiration_in_seconds, then the function returns
+  /// immediately and does not fetch any data. A cache_expiration_in_seconds of
+  /// zero will always cause a fetch.
+  ///
+  /// @return A Future which can be used to determine when the fetch is
+  /// complete.
+  Future<void> Fetch(uint64_t cache_expiration_in_seconds);
+
+  /// @brief Get the (possibly still pending) results of the most recent Fetch()
+  /// call.
+  ///
+  /// @return The future result from the last call to Fetch().
+  Future<void> FetchLastResult();
+
+#if defined(__ANDROID__)
+  /// @brief Sets the default values, using an XML resource.
+  ///
+  /// @note This method is specific to the Android implementation.
+  ///
+  /// This completely overwrites all previous default values.
+  ///
+  /// @param[in] defaults_resource_id Id for the XML resource, which should be
+  /// in your applications res/xml folder.
+  ///
+  /// @return a Future which can be used to determine when the operation is
+  /// complete.
+  Future<void> SetDefaults(int defaults_resource_id);
+#endif  // __ANDROID__
+
+#ifndef SWIG
+  /// @brief Sets the default values based on a mapping of string to Variant.
+  /// This allows you to specify defaults of type other than string.
+  ///
+  /// The type of each Variant in the map determines the type of data for which
+  /// you are providing a default. For example, boolean values can be retrieved
+  /// with GetBool(), integer values can be retrieved with GetLong(), double
+  /// values can be retrieved with GetDouble(), string values can be retrieved
+  /// with GetString(), and binary data can be retrieved with GetData().
+  /// Aggregate Variant types are not allowed.
+  ///
+  /// @see firebase::Variant for more information on how to create a Variant of
+  /// each type.
+  ///
+  /// @note This completely overrides all previous values.
+  ///
+  /// @param defaults Array of ConfigKeyValueVariant, representing the new set
+  /// of defaults to apply. If the same key is specified multiple times, the
+  /// value associated with the last duplicate key is applied.
+  /// @param number_of_defaults Number of elements in the defaults array.
+  ///
+  /// @return a Future which can be used to determine when the operation is
+  /// complete.
+  Future<void> SetDefaults(const ConfigKeyValueVariant* defaults,
+                           size_t number_of_defaults);
+#endif  // SWIG
+
+  /// @brief Sets the default values based on a string map.
+  ///
+  /// @note This completely overrides all previous values.
+  ///
+  /// @param defaults Array of ConfigKeyValue, representing the new set of
+  /// defaults to apply. If the same key is specified multiple times, the
+  /// value associated with the last duplicate key is applied.
+  /// @param number_of_defaults Number of elements in the defaults array.
+  ///
+  /// @return a Future which can be used to determine when the operation is
+  /// complete.
+  Future<void> SetDefaults(const ConfigKeyValue* defaults,
+                           size_t number_of_defaults);
+
+  /// @brief Get the (possibly still pending) results of the most recent
+  /// SetDefaults() call.
+  ///
+  /// @return The future result from the last call to SetDefaults().
+  Future<void> SetDefaultsLastResult();
+
+  /// @brief Asynchronously changes the settings for this Remote Config
+  /// instance.
+  ///
+  /// @param settings The new settings to be applied.
+  ///
+  /// @return a Future which can be used to determine when the operation is
+  /// complete.
+  Future<void> SetConfigSettings(ConfigSettings settings);
+
+  /// @brief Get the (possibly still pending) results of the most recent
+  /// SetConfigSettings() call.
+  ///
+  /// @return The future result from the last call to SetConfigSettings().
+  Future<void> SetConfigSettingsLastResult();
+
+  /// @brief Returns the value associated with a key, converted to a bool.
+  ///
+  /// Values of "1", "true", "t", "yes", "y" and "on" are interpreted (case
+  /// insensitive) as <code>true</code> and "0", "false", "f", "no", "n", "off",
+  /// and empty strings are interpreted (case insensitive) as
+  /// <code>false</code>.
+  ///
+  /// @param[in] key Key of the value to be retrieved.
+  ///
+  /// @return Value associated with the specified key converted to a boolean
+  /// value.
+  bool GetBoolean(const char* key);
+
+  /// @brief Returns the value associated with a key, converted to a bool.
+  ///
+  /// Values of "1", "true", "t", "yes", "y" and "on" are interpreted (case
+  /// insensitive) as <code>true</code> and "0", "false", "f", "no", "n", "off",
+  /// and empty strings are interpreted (case insensitive) as
+  /// <code>false</code>.
+  ///
+  /// @param[in] key Key of the value to be retrieved.
+  /// @param[out] info A return value, specifying the source of the returned
+  /// value.
+  ///
+  /// @return Value associated with the specified key converted to a boolean
+  /// value.
+  bool GetBoolean(const char* key, ValueInfo* info);
+
+  /// @brief Returns the value associated with a key, converted to a 64-bit
+  /// integer.
+  ///
+  /// @param[in] key Key of the value to be retrieved.
+  ///
+  /// @return Value associated with the specified key converted to a 64-bit
+  /// integer.
+  int64_t GetLong(const char* key);
+
+  /// @brief Returns the value associated with a key, converted to a 64-bit
+  /// integer.
+  ///
+  /// @param[in] key Key of the value to be retrieved.
+  /// @param[out] info A return value, specifying the source of the returned
+  /// value.
+  ///
+  /// @return Value associated with the specified key converted to a 64-bit
+  /// integer.
+  int64_t GetLong(const char* key, ValueInfo* info);
+
+  /// @brief Returns the value associated with a key, converted to a double.
+  ///
+  /// @param[in] key Key of the value to be retrieved.
+  ///
+  /// @return Value associated with the specified key converted to a double.
+  double GetDouble(const char* key);
+
+  /// @brief Returns the value associated with a key, converted to a double.
+  ///
+  /// @param[in] key Key of the value to be retrieved.
+  /// @param[out] info A return value, specifying the source of the returned
+  /// value.
+  ///
+  /// @return Value associated with the specified key converted to a double.
+  double GetDouble(const char* key, ValueInfo* info);
+
+  /// @brief Returns the value associated with a key, converted to a string.
+  ///
+  /// @param[in] key Key of the value to be retrieved.
+  ///
+  /// @return Value as a string associated with the specified key.
+  std::string GetString(const char* key);
+
+  /// @brief Returns the value associated with a key, converted to a string.
+  ///
+  /// @param[in] key Key of the value to be retrieved.
+  /// @param[out] info A return value, specifying the source of the returned
+  /// value.
+  ///
+  /// @return Value as a string associated with the specified key.
+  std::string GetString(const char* key, ValueInfo* info);
+
+  /// @brief Returns the value associated with a key, as a vector of raw
+  /// byte-data.
+  ///
+  /// @param[in] key Key of the value to be retrieved.
+  ///
+  /// @return Vector of bytes.
+  std::vector<unsigned char> GetData(const char* key);
+
+  /// @brief Returns the value associated with a key, as a vector of raw
+  /// byte-data.
+  ///
+  /// @param[in] key Key of the value to be retrieved.
+  /// @param[out] info A return value, specifying the source of the returned
+  /// value.
+  ///
+  /// @return Vector of bytes.
+  std::vector<unsigned char> GetData(const char* key, ValueInfo* info);
+
+  /// @brief Gets the set of keys that start with the given prefix.
+  ///
+  /// @param[in] prefix The key prefix to look for. If empty or null, this
+  /// method will return all keys.
+  ///
+  /// @return Set of Remote Config parameter keys that start with the specified
+  /// prefix. Will return an empty set if there are no keys with the given
+  /// prefix.
+  std::vector<std::string> GetKeysByPrefix(const char* prefix);
+
+  /// @brief Gets the set of all keys.
+  ///
+  /// @return Set of all Remote Config parameter keys.
+  std::vector<std::string> GetKeys();
+
+  /// @brief Returns a Map of Firebase Remote Config key value pairs.
+  ///
+  /// Evaluates the values of the parameters in the following order:
+  /// The activated value, if the last successful @ref Activate() contained the
+  /// key. The default value, if the key was set with @ref SetDefaults().
+  std::map<std::string, Variant> GetAll();
+
+  /// Gets the App this remote config object is connected to.
+  App* app() { return app_; }
+
+  /// Returns the RemoteConfig object for an App. Creates the RemoteConfig if
+  /// required.
+  ///
+  /// To get the RemoteConfig object for the default app, use,
+  /// GetInstance(GetDefaultFirebaseApp());
+  ///
+  /// If the library RemoteConfig fails to initialize, init_result_out will be
+  /// written with the result status (if a pointer is given).
+  ///
+  /// @param[in] app The App to use for the RemoteConfig object.
+  static RemoteConfig* GetInstance(App* app);
+
+ private:
+  RemoteConfig(App* app);
+
+  // Find RemoteConfig instance using App.  Return null if the instance does not
+  // exist.
+  static RemoteConfig* FindRemoteConfig(App* app);
+
+  // Clean up RemoteConfig instance.
+  static void DeleteInternal(RemoteConfig* rc);
+
+  /// The Firebase App this remote config is connected to.
+  App* app_;
+};
+
+#endif  // FIREBASE_EARLY_ACCESS_PREVIEW
 
 }  // namespace remote_config
 }  // namespace firebase
