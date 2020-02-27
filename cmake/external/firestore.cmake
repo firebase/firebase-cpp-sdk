396d8b4195271d67dbec621ce59f8577b24960d0
mcg <mcg@google.com>
2020-02-27 15:56:16 -0800

    Add an external download for Firestore
    PiperOrigin-RevId: 297709959

---
+++ cmake/external/firestore.cmake
@@ -0,0 +1,39 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include(ExternalProject)
+
+if(TARGET firestore)
+  return()
+endif()
+
+# Pin to the first revision that including
+# https://github.com/firebase/firebase-ios-sdk/pull/4984, that contains all the
+# fixes required to operate as an external build from within this repo.
+set(version d4e86ba091b0aeccfcdcf9d344338564f45c21c1)
+
+ExternalProject_Add(
+  firestore
+
+  DOWNLOAD_DIR ${FIREBASE_DOWNLOAD_DIR}
+  DOWNLOAD_NAME firestore-${version}.tar.gz
+  URL https://github.com/firebase/firebase-ios-sdk/archive/${version}.tar.gz
+
+  PREFIX ${PROJECT_BINARY_DIR}
+
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+  TEST_COMMAND ""
+)
