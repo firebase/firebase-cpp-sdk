c709dff4db688902c061c2851bbc8130a111c77b
varconst <varconst@google.com>
2020-04-24 15:16:10 -0700

    Use a single function, `FieldValue::Increment`, for both integer and double arguments.
    See go/firestore-cpp-field-value-increment for more background and rationale.
    
    This requires some template metaprogramming but results in a more convenient and safer API. In particular, `static_assert` allows preventing truncation with a readable error message (e.g. if a user passes `unsigned long long` as an argument).
    
    While working on this, I discovered that STLPort actually does contain `<type_traits>` header, so the STLPort implementation is pretty straightforward (the only extra complication is accounting for the nested `tr1` namespace). I checked the code from this CL manually in Android Studio using STLPort on NDK r17, and it compiled successfully.
    
    PiperOrigin-RevId: 308336176

--- app/meta/type_traits.h
+++ app/meta/type_traits.h
@@ -18,6 +18,7 @@
 #define FIREBASE_APP_CLIENT_CPP_META_TYPE_TRAITS_H_
 
 #include <cstdlib>
+#include <type_traits>
 
 #if !defined(FIREBASE_NAMESPACE)
 #define FIREBASE_NAMESPACE firebase
@@ -63,6 +64,57 @@ struct is_lvalue_reference {
 template <typename T>
 struct is_lvalue_reference<T&> {
   static constexpr bool value = true;
+};
+
+// STLPort does include <type_traits> header, but its contents are in `std::tr1`
+// namespace. To work around this, use aliases.
+// TODO(varconst): all of the reimplementations of traits above can be replaced
+// with appropriate aliases.
+// TODO(varconst): the traits in this file would be more conformant if they
+// inherited from `std::integral_constant`.
+#ifdef STLPORT
+#define FIREBASE_TYPE_TRAITS_NS std::tr1
+#else
+#define FIREBASE_TYPE_TRAITS_NS std
+#endif
+
+template <bool value, typename T = void>
+using enable_if = FIREBASE_TYPE_TRAITS_NS::enable_if<value, T>;
+
+template <typename T>
+using is_floating_point = FIREBASE_TYPE_TRAITS_NS::is_floating_point<T>;
+
+template <typename T>
+using is_integral = FIREBASE_TYPE_TRAITS_NS::is_integral<T>;
+
+template <typename T, typename U>
+using is_same = FIREBASE_TYPE_TRAITS_NS::is_same<T, U>;
+
+template <typename T, T value>
+using integral_constant = FIREBASE_TYPE_TRAITS_NS::integral_constant<T, value>;
+
+#undef FIREBASE_TYPE_TRAITS_NS
+
+// `is_char<T>::value` is true iff `T` is a character type (including `wchar_t`
+// and C++11 fixed-width character types).
+template <typename T>
+struct is_char {
+  static constexpr bool value =
+#if __cplusplus >= 202002L
+      is_same<T, char8_t>::value ||
+#endif
+#if __cplusplus >= 201103L
+      is_same<T, char16_t>::value || is_same<T, char32_t>::value ||
+#endif
+      is_same<T, char>::value || is_same<T, signed char>::value ||
+      is_same<T, unsigned char>::value || is_same<T, wchar_t>::value;
+};
+
+// A subset of `std::is_integral`: excludes `bool` and character types.
+template <typename T>
+struct is_integer {
+  static constexpr bool value =
+      is_integral<T>::value && !is_same<T, bool>::value && !is_char<T>::value;
 };
 
 // NOLINTNEXTLINE - allow namespace overridden
