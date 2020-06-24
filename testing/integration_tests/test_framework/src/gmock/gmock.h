77ff94ec4c0a40c95eb8b2a445af9630bb607ce6
Jon Simantov <jsimantov@google.com>
2020-06-24 13:28:54 -0700

    Add sample and test frameworks to integration_test directory.

---
+++ testing/integration_tests/test_framework/src/gmock/gmock.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FIREBASE_APP_CLIENT_CPP_TEST_FRAMEWORK_SRC_GMOCK_GMOCK_H_
+#define FIREBASE_APP_CLIENT_CPP_TEST_FRAMEWORK_SRC_GMOCK_GMOCK_H_
+
+/* This file exists so that blaze builds can include gmock/gmock.h (the public
+ * include path) rather than the internal include path. This file is omitted
+ * from the public repo. */
+
+#include "testing/base/public/gmock.h"
+
+#endif  // FIREBASE_APP_CLIENT_CPP_TEST_FRAMEWORK_SRC_GMOCK_GMOCK_H_
---
+++ testing/integration_tests/test_framework/src/gtest/gtest.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FIREBASE_APP_CLIENT_CPP_TEST_FRAMEWORK_SRC_GTEST_GTEST_H_
+#define FIREBASE_APP_CLIENT_CPP_TEST_FRAMEWORK_SRC_GTEST_GTEST_H_
+
+/* This file exists so that blaze builds can include gtest/gtest.h (the public
+ * include path) rather than the internal include path. This file is omitted
+ * from the public repo. */
+
+#include "testing/base/public/gunit.h"
+
+#endif  // FIREBASE_APP_CLIENT_CPP_TEST_FRAMEWORK_SRC_GTEST_GTEST_H_
