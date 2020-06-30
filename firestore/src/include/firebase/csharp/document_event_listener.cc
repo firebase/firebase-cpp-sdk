6815e02c9f9b4d6febe93c4b650853cf7a2eda8f
Googler <noreply@google.com>
2020-06-30 13:35:56 -0700

    Cleanup listener callbacks on `DocumentReference` and `Query`.
    This CL is the last in a series described in cl/312713181, cl/317397413, and resolves b/156024690.
    
    In this CL the new pattern for managing listener callbacks implemented in `FirebaseFirestore.ListenForSnapshotsInSync()` is being applied to all other places where we expose listeners (i.e. `Query` and `DocumentReference`).
    
    PiperOrigin-RevId: 319089024

--- firestore/src/include/firebase/csharp/document_event_listener.cc
+++ firestore/src/include/firebase/csharp/document_event_listener.cc
@@ -7,64 +7,25 @@ namespace firebase {
 namespace firestore {
 namespace csharp {
 
-::firebase::Mutex DocumentEventListener::g_mutex;
-DocumentEventListenerCallback
-    DocumentEventListener::g_document_snapshot_event_listener_callback =
-        nullptr;
-
 void DocumentEventListener::OnEvent(const DocumentSnapshot& value,
                                     Error error) {
-  MutexLock lock(g_mutex);
-  if (g_document_snapshot_event_listener_callback) {
-    firebase::callback::AddCallback(firebase::callback::NewCallback(
-        DocumentSnapshotEvent, callback_id_, value, error));
-  }
-}
+  // Ownership of this pointer is passed into the C# handler
+  auto* copy = new DocumentSnapshot(value);
 
-/* static */
-void DocumentEventListener::SetCallback(
-    DocumentEventListenerCallback callback) {
-  MutexLock lock(g_mutex);
-  if (!callback) {
-    g_document_snapshot_event_listener_callback = nullptr;
-    return;
-  }
-
-  if (g_document_snapshot_event_listener_callback) {
-    FIREBASE_ASSERT(g_document_snapshot_event_listener_callback == callback);
-  } else {
-    g_document_snapshot_event_listener_callback = callback;
-  }
+  callback_(callback_id_, copy, error);
 }
 
 /* static */
 ListenerRegistration DocumentEventListener::AddListenerTo(
-    int32_t callback_id, DocumentReference reference,
-    MetadataChanges metadata_changes) {
-  DocumentEventListener listener(callback_id);
-  // TODO(zxu): For now, we call the one with lambda instead of EventListener
-  // pointer so we do not have to manage the ownership of the listener. We
-  // might want to extend the design to manage listeners and call the one with
-  // EventListener parameter e.g. adding extra parameter in the API to specify
-  // whether ownership is transferred.
-  return reference.AddSnapshotListener(
+    DocumentReference* reference, MetadataChanges metadata_changes,
+    int32_t callback_id, DocumentEventListenerCallback callback) {
+  DocumentEventListener listener(callback_id, callback);
+
+  return reference->AddSnapshotListener(
       metadata_changes,
       [listener](const DocumentSnapshot& value, Error error) mutable {
         listener.OnEvent(value, error);
       });
-}
-
-/* static */
-void DocumentEventListener::DocumentSnapshotEvent(int callback_id,
-                                                  DocumentSnapshot value,
-                                                  Error error) {
-  MutexLock lock(g_mutex);
-  if (g_document_snapshot_event_listener_callback == nullptr) {
-    return;
-  }
-  // The ownership is passed through the call to C# handler.
-  DocumentSnapshot* copy = new DocumentSnapshot(value);
-  g_document_snapshot_event_listener_callback(callback_id, copy, error);
 }
 
 }  // namespace csharp
