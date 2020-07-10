40df6627ca0f7813a90e3b125a28fb95f3b18b66
Vimanyu <vimanyujain@google.com>
2020-07-10 12:49:44 -0700

    use EXPECT_LE instead of EXPECT_TRUE with a "less than" comparison
    We get much more descriptive logs in google tests on using EXPECT_LE
    instead of EXPECT_TRUE(x<y). With EXPECT_LE, the logs will print out
    the expected and actual value if the test fails. Super helpful for debugging.

--- app/tests/time_test.cc
+++ app/tests/time_test.cc
@@ -74,7 +74,7 @@ TEST(TimeTests, GetTimestampTest) {
 
   int64_t error = llabs(static_cast<int64_t>(end - start) - 500);
 
-  EXPECT_TRUE(error < 0.10 * firebase::internal::kMillisecondsPerSecond);
+  EXPECT_LT(error, 0.10 * firebase::internal::kMillisecondsPerSecond);
 }
 
 // Test GetTimestampEpoch function
@@ -95,7 +95,7 @@ TEST(TimeTests, GetTimestampEpochTest) {
   printf("%llu -> %llu (%lld)\n", start, end, error);
 #endif  // __linux__
 
-  EXPECT_TRUE(error < 0.10 * firebase::internal::kMillisecondsPerSecond);
+  EXPECT_LT(error, 0.10 * firebase::internal::kMillisecondsPerSecond);
 }
 
 }  // namespace
