9d81cc68f29ec3dffb24e67167b3c375f6b45f31
Vimanyu Jain <vimanyujain@google.com>
2020-07-30 14:46:49 -0700

    Feature/prereqs build scripts redesign (#102)
    * simplified with no cmd line arguments
    
    prereqs script sets up dependencies in a platform aganostic manner.
    installs ccache on mac/linux and installs python dependencies
    
    * python3 goodness and added some helper functions
    
    * docstring for unpacking files
    
    * Python3 script to build cpp sdk
    
    installs cpp deps via vcpkg, cmake configure and build
    
    * removed option for specifying build generator
    
    Google style 2 spaces indentation
    Removed support for generator
    Removed flag -j
    Fixed architecture flag being passed to various steps
    
    * added push trigger and removed ninja
    
    Removing Ninja allowed us to remove generators altogether from workflow file
    and simplified a number of steps
    
    * fixed call to undefined function
    
    * removing push trigger
    
    * moved files to gha directory
    
    * addressed PR notes
    
    shared functions in separate module
    simplified and moving more towards shell script style python script
    using python3 constucts for better code
    
    * improved readability and correctness of build command
    
    * updated python script paths
    
    * upgrade python 3.6 to 3.7
    
    subprocess.run, capture_output flag was added in 3.7
    
    * removing env var for pip requirements
    
    * dummy commit to trigger workflow
    
    PR merge is blocked because its waiting on status from tasks that no longer exist
    Testing if a new commit will help

---
+++ scripts/gha/build_desktop.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+
+# Copyright 2020 Google
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Assuming pre-requisites are in place (from running 
+`scripts/gha/install_prereqs_desktop.py`), this builds the firebase cpp sdk.
+
+It does the following,
+- install desktop platform specific cpp dependencies via vcpkg
+- cmake configure
+- cmake build
+
+Usage examples:
+# Build all targets with x64 architecture, Release config and 8 cpu cores
+python scripts/gha/build_desktop.py --arch x64 --config Release -j 8
+
+# Build all targets with default options and also build unit tests
+python scripts/gha/build_desktop.py --build_tests --arch x64
+
+# Build only firebase_app and firebase_auth 
+python scripts/gha/build_desktop.py --target firebase_app firebase_auth
+
+"""
+
+import argparse
+import os
+import utils
+
+
+def install_cpp_dependencies_with_vcpkg(arch):
+  """Install packages with vcpkg.
+
+  This does the following,
+    - initialize/update the vcpkg git submodule
+    - build vcpkg executable
+    - install packages via vcpkg.
+  Args:
+    arch (str): Architecture (eg: 'x86', 'x64').
+  """
+
+  # Install vcpkg executable if its not installed already
+  vcpkg_executable_file_path = utils.get_vcpkg_executable_file_path()
+  found_vcpkg_executable = os.path.exists(vcpkg_executable_file_path)
+  if not found_vcpkg_executable:
+    script_absolute_path = utils.get_vcpkg_installation_script_path()
+    # Example: ./external/vcpkg/bootstrap-sh
+    utils.run_command([script_absolute_path])
+
+  # for each desktop platform, there exists a vcpkg response file in the repo
+  # (external/vcpkg_<triplet>_response_file.txt) defined for each target triplet
+  vcpkg_triplet = utils.get_vcpkg_triplet(arch)
+  vcpkg_response_file_path = os.path.join(os.getcwd(), 'external', 
+                      'vcpkg_' + vcpkg_triplet + '_response_file.txt')
+
+  # Eg: ./external/vcpkg/vcpkg install @external/vcpkg_x64-osx_response_file.txt 
+  # --disable-metrics
+  utils.run_command([vcpkg_executable_file_path, 'install',
+                     '@' + vcpkg_response_file_path, '--disable-metrics'])
+
+
+def cmake_configure(build_dir, arch, build_tests=True, config=None):
+  """ CMake configure.
+
+  If you are seeing problems when running this multiple times,
+  make sure to clean/delete previous build directory.
+
+  Args:
+   build_dir (str): Output build directory.
+   arch (str): Platform Architecture (example: 'x64').
+   build_tests (bool): Build cpp unit tests.
+   config (str): Release/Debug config.
+          If its not specified, cmake's default is used (most likely Debug).
+  """   
+  cmd = ['cmake', '-S', '.', '-B', build_dir]
+  
+  # If generator is not specifed, default for platform is used by cmake, else 
+  # use the specified value
+  if config:
+    cmd.append('-DCMAKE_BUILD_TYPE={0}'.format(config))
+  if build_tests:
+    cmd.append('-DFIREBASE_CPP_BUILD_TESTS=ON')
+    cmd.append('-DFIREBASE_FORCE_FAKE_SECURE_STORAGE=ON')
+
+  vcpkg_toolchain_file_path = os.path.join(os.getcwd(), 'external',
+                                           'vcpkg', 'scripts', 
+                                           'buildsystems', 'vcpkg.cmake')
+  cmd.append('-DCMAKE_TOOLCHAIN_FILE={0}'.format(vcpkg_toolchain_file_path))
+
+  vcpkg_triplet = utils.get_vcpkg_triplet(arch)
+  cmd.append('-DVCPKG_TARGET_TRIPLET={0}'.format(vcpkg_triplet))
+  
+  # TODO: Remove this once firestore is included in the build and everything works
+  cmd.append('-DFIREBASE_INCLUDE_FIRESTORE=OFF')
+  utils.run_command(cmd)
+
+
+def main():
+  args = parse_cmdline_args()
+  
+  # Ensure that the submodules are initialized and updated
+  # Example: vcpkg is a submodule (external/vcpkg)
+  utils.run_command(['git', 'submodule', 'init'])
+  utils.run_command(['git', 'submodule', 'update'])
+
+  # Install platform dependent cpp dependencies with vcpkg 
+  install_cpp_dependencies_with_vcpkg(args.arch)
+
+  # CMake configure
+  cmake_configure(args.build_dir, args.arch, args.build_tests, args.config)
+
+  # CMake build 
+  # cmake --build build -j 8 
+  cmd = ['cmake', '--build', args.build_dir, '-j', str(os.cpu_count())] 
+  if args.target:
+    # Example:  cmake --build build -j 8 --target firebase_app firebase_auth
+    cmd.append('--target')
+    cmd.extend(args.target)
+  utils.run_command(cmd)
+
+
+def parse_cmdline_args():
+  parser = argparse.ArgumentParser(description='Install Prerequisites for building cpp sdk')
+  parser.add_argument('-a', '--arch', default='x64', help='Platform architecture (x64, x86)')
+  parser.add_argument('--build_dir', default='build', help='Output build directory')
+  parser.add_argument('--build_tests', action='store_true', help='Build unit tests too')
+  parser.add_argument('--config', help='Release/Debug config')
+  parser.add_argument('--target', nargs='+', help='A list of CMake build targets (eg: firebase_app firebase_auth)')
+  args = parser.parse_args()
+  return args
+
+if __name__ == '__main__':
+  main()
+
---
+++ scripts/gha/utils.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+
+# Copyright 2020 Google
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Helper functions that are shared amongst prereqs and build scripts across various
+platforms.
+"""
+
+import distutils.spawn
+import platform
+import shutil
+import subprocess
+import os
+import urllib.request
+
+def run_command(cmd, capture_output=False, cwd=None, check=False, as_root=False):
+ """Run a command.
+
+ Args:
+  cmd (list(str)): Command to run as a list object.
+          Eg: ['ls', '-l'].
+  capture_output (bool): Capture the output of this command.
+                         Output can be accessed as <return_object>.stdout
+  cwd (str): Directory to execute the command from.
+  check (bool): Raises a CalledProcessError if True and the command errored out
+  as_root (bool): Run command as root user with admin priveleges (supported on mac and linux).
+
+ Raises:
+  (subprocess.CalledProcessError): If command errored out and `text=True`
+
+ Returns:
+  (`subprocess.CompletedProcess`): object containing information from command execution
+ """
+
+ if as_root and (is_mac_os() or is_linux_os()):
+  cmd.insert(0, 'sudo')
+
+ cmd_string = ' '.join(cmd)
+ print('Running cmd: {0}\n'.format(cmd_string))
+ # If capture_output is requested, we also set text=True to store the returned value of the
+ # command as a string instead of bytes object
+ return subprocess.run(cmd, capture_output=capture_output, cwd=cwd, check=check, text=capture_output)
+
+
+def is_command_installed(tool):
+  """Check if a command is installed on the system."""
+  return distutils.spawn.find_executable(tool)
+
+
+def download_file(url, file_path):
+  """Download from url and save to specified file path."""
+  with urllib.request.urlopen(url) as response, open(file_path, 'wb') as out_file:
+    shutil.copyfileobj(response, out_file)
+
+
+def unpack_files(archive_file_path, output_dir=None):
+  """Unpack/extract an archive to specified output_directory"""
+  shutil.unpack_archive(archive_file_path, output_dir)
+
+
+def is_windows_os():
+ return platform.system() == 'Windows'
+
+
+def is_mac_os():
+ return platform.system() == 'Darwin'
+
+
+def is_linux_os():
+ return platform.system() == 'Linux'
+
+
+def get_vcpkg_triplet(arch):
+  """ Get vcpkg target triplet (platform definition).
+
+  Args:
+    arch (str): Architecture (eg: 'x86', 'x64').
+
+  Raises:
+    ValueError: If current OS is not win,mac or linux.
+  
+  Returns:
+    (str): Triplet name.
+       Eg: "x64-windows-static".
+  """
+  triplet_name = [arch]
+  if is_windows_os():
+    triplet_name.append('windows')
+    # For windows, default is to build dynamic. Hence we specify static.
+    # For mac/linux, default is to build static libraries
+    triplet_name.append('static')
+  elif is_mac_os():
+    triplet_name.append('osx')
+  elif is_linux_os():
+    triplet_name.append('linux')
+
+  triplet_name = '-'.join(triplet_name)
+  print("Using vcpkg triplet: {0}".format(triplet_name))
+  return triplet_name
+
+
+def get_vcpkg_executable_file_path():
+  """Get absolute path to vcpkg executable."""
+  vcpkg_root_dir = os.path.join(os.getcwd(), 'external', 'vcpkg')
+  if is_windows_os():
+    vcpkg_executable_file_path = os.path.join(vcpkg_root_dir, 'vcpkg.exe')
+  elif is_linux_os() or is_mac_os():
+    vcpkg_executable_file_path = os.path.join(vcpkg_root_dir, 'vcpkg')
+  return vcpkg_executable_file_path
+
+
+def get_vcpkg_installation_script_path():
+  """Get absolute path to the script used to build and install vcpkg."""
+  vcpkg_root_dir = os.path.join(os.getcwd(), 'external', 'vcpkg')
+  if is_windows_os():
+    script_absolute_path = os.path.join(vcpkg_root_dir, 'bootstrap-vcpkg.bat')
+  elif is_linux_os() or is_mac_os():
+    script_absolute_path = os.path.join(vcpkg_root_dir, 'bootstrap-vcpkg.sh')
+
+  return script_absolute_path
