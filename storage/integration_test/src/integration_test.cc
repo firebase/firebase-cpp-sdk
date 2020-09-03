3ece8b2f28ade78f6b8991833d01544ac31d4866
DellaBitta <DellaBitta@users.noreply.github.com>
2020-09-03 17:52:00 -0400

    Fix Storage Long Download Pause n' Cancel Integration Test (#132)
    * smaller ticks
    
    * added newline to the end of the file
    
    * update comments about testing for successful pause

--- storage/integration_test/src/integration_test.cc
+++ storage/integration_test/src/integration_test.cc
@@ -588,11 +588,14 @@ TEST_F(FirebaseStorageTest, TestLargeFilePauseResumeAndDownloadCancel) {
     // operation.
     ASSERT_TRUE(controller.is_valid());
 
-    ProcessEvents(500);
-    // After waiting a moment for the operation to start, pause the operation
-    // and verify it was successfully paused. Note that pause might not take
-    // effect immediately, so we give it a few moments to pause before
-    // failing.
+    while(controller.bytes_transferred() == 0) 
+    {
+      ProcessEvents(1);
+    }
+    
+    // After waiting a moment for the operation to start (above), pause the
+    // operation and verify it was successfully paused when the future
+    // completes.
     LogDebug("Pausing upload.");
     EXPECT_TRUE(controller.Pause()) << "Upload pause";
 
@@ -638,7 +641,11 @@ TEST_F(FirebaseStorageTest, TestLargeFilePauseResumeAndDownloadCancel) {
         ref.GetBytes(&buffer[0], kLargeFileSize, &listener, &controller);
     ASSERT_TRUE(controller.is_valid());
 
-    ProcessEvents(500);
+    while(controller.bytes_transferred() == 0) 
+    {
+      ProcessEvents(1);
+    }
+
     LogDebug("Pausing download.");
     EXPECT_TRUE(controller.Pause()) << "Download pause";
 
@@ -691,7 +698,12 @@ TEST_F(FirebaseStorageTest, TestLargeFilePauseResumeAndDownloadCancel) {
     firebase::Future<size_t> future =
         ref.GetBytes(&buffer[0], kLargeFileSize, &listener, &controller);
     ASSERT_TRUE(controller.is_valid());
-    ProcessEvents(500);
+    
+    while(controller.bytes_transferred() == 0) 
+    {
+      ProcessEvents(1);
+    }
+    
     LogDebug("Cancelling download.");
     EXPECT_TRUE(controller.Cancel());
     WaitForCompletion(future, "GetBytes", firebase::storage::kErrorCancelled);
@@ -717,8 +729,11 @@ TEST_F(FirebaseStorageTest, TestLargeFileCancelUpload) {
     // operation.
     ASSERT_TRUE(controller.is_valid());
 
-    ProcessEvents(500);
-
+    while(controller.bytes_transferred() == 0) 
+    {
+      ProcessEvents(1);
+    }
+    
     LogDebug("Cancelling upload.");
     // Cancel the operation and verify it was successfully canceled.
     EXPECT_TRUE(controller.Cancel());
