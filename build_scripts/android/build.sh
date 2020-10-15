ba5d961ca73add4c16427c41a5036a070eccbea0
Jon Simantov <jsimantov@google.com>
2020-10-15 13:58:50 -0700

    Add STL build implementation.

--- build_scripts/android/build.sh
+++ build_scripts/android/build.sh
@@ -1,11 +1,11 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 buildpath=$1
 sourcepath=$2
 stl=$3
 
 if [[ -z "${buildpath}" || -z "${sourcepath}" ]]; then
-    echo "Usage: $0 <build path> <source path> [libc++|stdc++|stlport]"
+    echo "Usage: $0 <build path> <source path> [c++|gnustl|stlport]"
     exit 1
 fi
 
@@ -14,13 +14,11 @@ if [[ ! -d "${sourcepath}" ]]; then
     exit 2
 fi
 
-if [[ ! -z "${stl}" ]]; then
-    stl="libc++"
-fi
-
-if [[ "${stl}" != "libc++" && "${stl}" != "stdc++" && "${stl}" != "stlport" ]]; then
+if [[ "${stl}" == "c++" || "${stl}" == "gnustl" || "${stl}" == "stlport" ]]; then
+    export FIREBASE_ANDROID_STL="${stl}"_static
+elif [[ ! -z "${stl}" ]]; then
     echo "Invalid STL specified."
-    echo "Valid STLs are: 'libc++' (default), 'stdc++', or 'stlport'"
+    echo "Valid STLs are: 'c++' (default), 'gnustl', or 'stlport'"
     exit 2
 fi
 
@@ -38,9 +36,19 @@ cd -
 # If NDK_ROOT is not set or is the wrong version, use to the version in /tmp.
 if [[ -z "${NDK_ROOT}" || ! $(grep -q "Pkg\.Revision = 16\." "${NDK_ROOT}/source.properties") ]]; then
     if [[ ! -d /tmp/android-ndk-r16b ]]; then
-	echo "NDK r16b not present in /tmp, please run install_prereqs.sh script."
+	echo "Recommended NDK version r16b not present in /tmp."
+	if [[ ! -z "${stl}" ]]; then
+	    echo "STL may only be specified if using the recommended NDK version."
+	    echo "Please run install_prereqs.sh script and try again."
+	    exit 2
+	else
+	    echo "Please run install_prereqs.sh if you wish to use the recommended NDK version."
+	    echo "Continuing with default NDK..."
+	    sleep 2
+	fi
     fi
     export NDK_ROOT=/tmp/android-ndk-r16b
+    export ANDROID_NDK_HOME=/tmp/android-ndk-r16b
 fi
 set -ex
 cd "${sourcepath}"
--- build_scripts/ios/build.sh
+++ build_scripts/ios/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 buildpath=$1
 sourcepath=$2
