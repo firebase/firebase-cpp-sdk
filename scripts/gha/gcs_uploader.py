eb138e5b4f6d603c1a82348a517f291854cceac3
anonymous-akorn <66133366+anonymous-akorn@users.noreply.github.com>
2020-10-20 12:46:03 -0700

    Add copyright and license headers to script files.

--- scripts/gha/gcs_uploader.py
+++ scripts/gha/gcs_uploader.py
@@ -1,3 +1,17 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Command line tool for submitting artifacts to Google Cloud Storage.
 
 Example usage:
@@ -74,8 +88,8 @@ def main(argv):
     subprocess.run([gcs.GSUTIL, "cp", artifact, dest], check=True)
   logging.info("Finished uploading to %s", gcs_prefix)
   logging.info(
-      "Use 'gsutil cp <gs_uri> <local_path> to copy an artifact locally.\n"
-      "Use 'gsutil -m cp -r %s <local_path> to copy everything.", gcs_prefix)
+      "Use 'gsutil cp <gs_uri> <local_path>' to copy an artifact locally.\n"
+      "Use 'gsutil -m cp -r %s <local_path>' to copy everything.", gcs_prefix)
 
 
 def _local_path_to_gcs_uri(gcs_prefix, path, testapp_dir):
