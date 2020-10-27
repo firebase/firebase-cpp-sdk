9b4b4ed7e248778a08ded4ae18e796bfd3535b7e
Vimanyu <vimanyujain@google.com>
2020-10-27 15:54:31 -0700

    added cmd line flags

--- scripts/gha/inspect_built_libraries.py
+++ scripts/gha/inspect_built_libraries.py
@@ -3,6 +3,8 @@ import sys
 import re
 import tempfile
 import subprocess
+import argparse
+import functools
 
 import utils
 
@@ -14,7 +16,7 @@ MSVC_RUNTIME_LIBRARIES_MAP = {
         'LIBCMTD': '/MTd'
         }
 
-def get_libraries_to_inspect(paths):
+def get_libraries_to_inspect(paths, firebase_only=True):
   libraries = []
   for path in paths:
     if os.path.isdir(path):
@@ -22,14 +24,18 @@ def get_libraries_to_inspect(paths):
         for filename in filenames:
           _name, ext = os.path.splitext(filename)
           if ext in LIBRARY_EXTENSIONS:
+            if firebase_only and 'firebase_' not in filename:
+              continue
             libraries.append(os.path.join(dirpath, filename))
     else:
       abspath = os.path.abspath(path)
       if os.path.exists(abspath):
-        _name, ext = os.path.splitext(path)
+        name, ext = os.path.splitext(path)
         if ext in LIBRARY_EXTENSIONS:
+          if firebase_only and 'firebase_' not in name:
+            continue
           libraries.append(abspath)
-    return libraries
+  return libraries
 
 def get_dumpbin_exe_path():
   tempdir = tempfile.gettempdir()
@@ -39,7 +45,7 @@ def get_dumpbin_exe_path():
   utils.download_file('https://github.com/microsoft/vswhere/releases/download/2.8.4/vswhere.exe', tempfile_path)
 
   output = utils.run_command([tempfile_path, '-latest', '-property', 'installationPath'],
-                             capture_output=True)
+                             capture_output=True, print_cmd=False)
 
   msvc_dir = os.path.join(output.stdout.replace('\n', ''), 'VC', 'Tools', 'MSVC')
   version_dir = os.listdir(msvc_dir)[0]
@@ -104,7 +110,7 @@ def get_arch_linux(lib, re_pattern=None):
   if re_pattern is None:
     re_pattern = get_arch_re_pattern_linux()
 
-  output = utils.run_command(['objdump', '-f', lib], capture_output=True)
+  output = utils.run_command(['objdump', '-f', lib], capture_output=True, print_cmd=False)
   lines = output.stdout.splitlines()
   object_files = {}
   for idx,line in enumerate(lines):
@@ -127,7 +133,7 @@ def get_arch_mac(lib, re_pattern=None):
     print("ERROR: get_arch_mac function can only be called on macs. Exiting ")
     sys.exit(1)
 
-  output = utils.run_command(['lipo', '-info', lib], capture_output=True)
+  output = utils.run_command(['lipo', '-info', lib], capture_output=True, print_cmd=False)
   if re_pattern is None:
     re_pattern = get_arch_re_pattern_mac()
 
@@ -138,15 +144,42 @@ def get_arch_mac(lib, re_pattern=None):
 
 
 def main():
-  libs = get_libraries_to_inspect([sys.argv[1]])
+  args = parse_cmdline_args()
+
+  if utils.is_linux_os():
+    pattern = get_arch_re_pattern_linux()
+    arch_function = functools.paget_arch_linux
+  elif utils.is_mac_os():
+    pattern = get_arch_re_pattern_mac()
+    arch_function = get_arch_mac
+  else:
+    pattern = get_arch_re_pattern_windows()
+    dumpbin = get_dumpbin_exe_path()
+    arch_function = get_arch_windows
+
+  libs = get_libraries_to_inspect(args.libraries, not args.all)
   for lib in libs:
     print(lib)
-    dumpbin = get_dumpbin_exe_path()
-    arch = get_arch_windows(lib, dumpbin)
-    rl = get_msvc_runtime_linkage(lib, dumpbin)
+    # dumpbin = get_dumpbin_exe_path()
+    # arch = get_arch_windows(lib, dumpbin)
+    # rl = get_msvc_runtime_linkage(lib, dumpbin)
+    arch = get_arch_mac(lib)
     print(arch)
-    print(rl)
 
+def parse_cmdline_args():
+  parser = argparse.ArgumentParser(description='Inspect prebuilt libraries/archives '
+                                               'for architecture (x64, x86) '\
+                                               'and msvc runtime linkage (/MD vs /MT).')
+  parser.add_argument('libraries', metavar='L', nargs='+',
+                    help='List of files (libraries) and/or directories (containing libraries).')
+  parser.add_argument('--all', action='store_true',
+                      help='Inspect all libraries found under specified directory.')
+  parser.add_argument('--print_full_paths', action='store_true',
+                      help='Print full library paths in summary.')
+  parser.add_argument('--verbose', action='store_true',
+                      help='Print all relevant output as it is (without summarizing).')
+  args = parser.parse_args()
+  return args
 
 if __name__ == '__main__':
   main()
