f4e885832d7b5829327735b3364d269d7f530938
amaurice <amaurice@google.com>
2020-11-20 16:28:29 -0800

    Remove unnecessary std before strlen, as it causes problems
    PiperOrigin-RevId: 343586290

--- app/src/filesystem_desktop_linux.cc
+++ app/src/filesystem_desktop_linux.cc
@@ -76,7 +76,7 @@ bool Mkdir(const std::string& path, std::string* out_error) {
 
 std::string AppDataDir(const char* app_name, bool should_create,
                        std::string* out_error) {
-  if (!app_name || std::strlen(app_name) == 0) {
+  if (!app_name || strlen(app_name) == 0) {
     if (out_error) {
       *out_error = "AppDataDir failed: no app_name provided";
     }
