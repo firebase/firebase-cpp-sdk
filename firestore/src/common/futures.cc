0fea62e8526deb7fa760060ff19c3e4af5e44946
mcg <mcg@google.com>
2021-01-26 11:05:38 -0800

    Use the function registry for Auth calls
    This makes Firebase Auth functionally a soft dependency of Firestore, but does
    not include the BUILD changes required to affect that.
    
    PiperOrigin-RevId: 353905031

---
+++ firestore/src/common/futures.cc
@@ -0,0 +1,15 @@
+#include "firestore/src/common/futures.h"
+
+namespace firebase {
+namespace firestore {
+namespace internal {
+
+ReferenceCountedFutureImpl* GetSharedReferenceCountedFutureImpl() {
+  static auto* futures =
+      new ReferenceCountedFutureImpl(/*last_result_count=*/0);
+  return futures;
+}
+
+}  // namespace internal
+}  // namespace firestore
+}  // namespace firebase
