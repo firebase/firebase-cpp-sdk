045e45771fe29bdc6e7ee42eb3e97e209de03fb4
mcg <mcg@google.com>
2021-01-29 10:40:02 -0800

    Port exception-throwing abstractions from iOS to common
    Unfortunately, because we still support STLPort on Android, this requires
    special casing there. On desktop platforms this delegates back to the iOS core.
    
    This includes a change to the iOS firestore_exceptions.h which needs to be
    ported to GitHub before the next import. It seems as if policy checks in Abseil
    fail on Android even when compiling against non-STLPort C++ runtimes.
    
    PiperOrigin-RevId: 354562822

--- firestore/src/ios/credentials_provider_desktop.h
+++ firestore/src/ios/credentials_provider_desktop.h
@@ -24,7 +24,7 @@ class FirebaseCppCredentialsProvider
     : public firestore::auth::CredentialsProvider {
  public:
   explicit FirebaseCppCredentialsProvider(App& app);
-  ~FirebaseCppCredentialsProvider();
+  ~FirebaseCppCredentialsProvider() override;
 
   FirebaseCppCredentialsProvider(const FirebaseCppCredentialsProvider&) =
       delete;
