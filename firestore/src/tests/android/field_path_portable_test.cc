07714dd0ff5df97261ebcf666a3127e34018e885
mcg <mcg@google.com>
2021-01-29 13:44:06 -0800

    Make field_path_portable_test work on Android
    Also, remove server-format parsing from this implementation. FieldPathPortable
    can only ever take values from users.
    
    PiperOrigin-RevId: 354602321

--- firestore/src/tests/android/field_path_portable_test.cc
+++ firestore/src/tests/android/field_path_portable_test.cc
@@ -1,9 +1,12 @@
 #include "firestore/src/android/field_path_portable.h"
 
+#include "firestore/src/common/macros.h"
 #include "gtest/gtest.h"
 
 namespace firebase {
 namespace firestore {
+
+#if defined(__ANDROID__)
 
 // The test cases are copied from
 //     Firestore/core/test/firebase/firestore/model/field_path_test.cc
@@ -61,50 +64,6 @@ TEST(FieldPath, CanonicalStringEscaping) {
   EXPECT_EQ(FieldPathPortable({"a_"}).CanonicalString(), "a_");
 }
 
-TEST(FieldPathPortableTest, Parsing) {
-  EXPECT_EQ(FieldPathPortable::FromServerFormat("foo"),
-            FieldPathPortable({"foo"}));
-  EXPECT_EQ(FieldPathPortable::FromServerFormat("foo.bar"),
-            FieldPathPortable({"foo", "bar"}));
-  EXPECT_EQ(FieldPathPortable::FromServerFormat("foo.bar.baz"),
-            FieldPathPortable({"foo", "bar", "baz"}));
-  EXPECT_EQ(FieldPathPortable::FromServerFormat(R"(`.foo\\`)"),
-            FieldPathPortable({".foo\\"}));
-  EXPECT_EQ(FieldPathPortable::FromServerFormat(R"(`.foo\\`.`.foo`)"),
-            FieldPathPortable({".foo\\", ".foo"}));
-  EXPECT_EQ(FieldPathPortable::FromServerFormat(R"(foo.`\``.bar)"),
-            FieldPathPortable({"foo", "`", "bar"}));
-  EXPECT_EQ(FieldPathPortable::FromServerFormat(R"(foo\.bar)"),
-            FieldPathPortable({"foo.bar"}));
-}
-
-// This is a special case in C++: std::string may contain embedded nulls. To
-// fully mimic behavior of Objective-C code, parsing must terminate upon
-// encountering the first null terminator in the string.
-TEST(FieldPathPortableTest, ParseEmbeddedNull) {
-  std::string str{"foo"};
-  str += '\0';
-  str += ".bar";
-
-  const auto path = FieldPathPortable::FromServerFormat(str);
-  EXPECT_EQ(path.size(), 1u);
-  EXPECT_EQ(path.CanonicalString(), "foo");
-}
-
-TEST(FieldPathPortableDeathTest, ParseFailures) {
-  EXPECT_DEATH(FieldPathPortable::FromServerFormat(""), "");
-  EXPECT_DEATH(FieldPathPortable::FromServerFormat("."), "");
-  EXPECT_DEATH(FieldPathPortable::FromServerFormat(".."), "");
-  EXPECT_DEATH(FieldPathPortable::FromServerFormat("foo."), "");
-  EXPECT_DEATH(FieldPathPortable::FromServerFormat(".bar"), "");
-  EXPECT_DEATH(FieldPathPortable::FromServerFormat("foo..bar"), "");
-  EXPECT_DEATH(FieldPathPortable::FromServerFormat(R"(foo\)"), "");
-  EXPECT_DEATH(FieldPathPortable::FromServerFormat(R"(foo.\)"), "");
-  EXPECT_DEATH(FieldPathPortable::FromServerFormat("foo`"), "");
-  EXPECT_DEATH(FieldPathPortable::FromServerFormat("foo```"), "");
-  EXPECT_DEATH(FieldPathPortable::FromServerFormat("`foo"), "");
-}
-
 TEST(FieldPathPortableTest, FromDotSeparatedString) {
   EXPECT_EQ(FieldPathPortable::FromDotSeparatedString("a"),
             FieldPathPortable({"a"}));
@@ -118,23 +77,40 @@ TEST(FieldPathPortableTest, FromDotSeparatedString) {
             FieldPathPortable({"foo", "bar", "baz"}));
 }
 
+#if FIRESTORE_HAVE_EXCEPTIONS
+// When exceptions are disabled, invalid input will crash the process, but our
+// gtest integration into the Java testing harness makes this impossible to
+// record.
+
 TEST(FieldPathPortableDeathTest, FromDotSeparatedStringParseFailures) {
-  EXPECT_DEATH(FieldPathPortable::FromDotSeparatedString(""), "");
-  EXPECT_DEATH(FieldPathPortable::FromDotSeparatedString("."), "");
-  EXPECT_DEATH(FieldPathPortable::FromDotSeparatedString(".foo"), "");
-  EXPECT_DEATH(FieldPathPortable::FromDotSeparatedString("foo."), "");
-  EXPECT_DEATH(FieldPathPortable::FromDotSeparatedString("foo..bar"), "");
+  EXPECT_THROW(FieldPathPortable::FromDotSeparatedString(""),
+               std::invalid_argument);
+  EXPECT_THROW(FieldPathPortable::FromDotSeparatedString("."),
+               std::invalid_argument);
+  EXPECT_THROW(FieldPathPortable::FromDotSeparatedString(".."),
+               std::invalid_argument);
+  EXPECT_THROW(FieldPathPortable::FromDotSeparatedString(".foo"),
+               std::invalid_argument);
+  EXPECT_THROW(FieldPathPortable::FromDotSeparatedString("foo."),
+               std::invalid_argument);
+  EXPECT_THROW(FieldPathPortable::FromDotSeparatedString("foo..bar"),
+               std::invalid_argument);
 }
+
+#endif  // FIRESTORE_HAVE_EXCEPTIONS
 
 TEST(FieldPathPortableTest, KeyFieldPath) {
   const auto& key_field_path = FieldPathPortable::KeyFieldPath();
   EXPECT_TRUE(key_field_path.IsKeyFieldPath());
   EXPECT_EQ(key_field_path, FieldPathPortable{key_field_path});
   EXPECT_EQ(key_field_path.CanonicalString(), "__name__");
-  EXPECT_EQ(key_field_path, FieldPathPortable::FromServerFormat("__name__"));
-  EXPECT_NE(key_field_path, FieldPathPortable::FromServerFormat(
+  EXPECT_EQ(key_field_path,
+            FieldPathPortable::FromDotSeparatedString("__name__"));
+  EXPECT_NE(key_field_path, FieldPathPortable::FromDotSeparatedString(
                                 key_field_path.CanonicalString().substr(1)));
 }
+
+#endif  // defined(__ANDROID__)
 
 }  // namespace firestore
 }  // namespace firebase
