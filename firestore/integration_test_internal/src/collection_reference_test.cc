9da7bcc73fbf88b279af7f24670458b6ea7f7ecc
Vimanyu <vimanyujain@google.com>
2021-03-17 17:37:02 -0700

    added collection_reference_test
    This test is active only on Android or Firestore stub build.

--- firestore/integration_test_internal/src/collection_reference_test.cc
+++ firestore/integration_test_internal/src/collection_reference_test.cc
@@ -1,14 +1,14 @@
 #include <utility>
 
 #include "firestore/src/common/wrapper_assertions.h"
-#include "firestore/src/include/firebase/firestore.h"
+#include "firebase/firestore.h"
 #if defined(__ANDROID__)
 #include "firestore/src/android/collection_reference_android.h"
 #elif defined(FIRESTORE_STUB_BUILD)
 #include "firestore/src/stub/collection_reference_stub.h"
 #endif  // defined(__ANDROID__)
 
-#include "testing/base/public/gmock.h"
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 namespace firebase {
