6d9690fd2597d83d99ec6c9e3b9a658e0577a45e
Vimanyu <vimanyujain@google.com>
2021-03-17 17:41:13 -0700

    added cursor_test

--- firestore/integration_test_internal/src/cursor_test.cc
+++ firestore/integration_test_internal/src/cursor_test.cc
@@ -1,9 +1,9 @@
 #include <string>
 #include <vector>
 
-#include "firestore/src/include/firebase/firestore.h"
-#include "firestore/src/tests/firestore_integration_test.h"
-#include "testing/base/public/gmock.h"
+#include "firebase/firestore.h"
+#include "firestore_integration_test.h"
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 // These test cases are in sync with native iOS client SDK test
