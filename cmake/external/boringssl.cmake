e61a140f5442e42bfd1f3287085c79e84e803d31
drsanta@google.com <drsanta@google.com>
2021-06-22 15:12:59 -0400

    use boring ssl commit tag variable

--- cmake/external/boringssl.cmake
+++ cmake/external/boringssl.cmake
@@ -21,14 +21,14 @@ endif()
 set(patch_file 
   ${CMAKE_CURRENT_LIST_DIR}/../../scripts/git/patches/boringssl/0001-disable-C4255-converting-empty-params-to-void.patch)
 
-set(commit_tag, 83da28a68f32023fd3b95a8ae94991a07b1f6c62)
+set(commit_tag 83da28a68f32023fd3b95a8ae94991a07b1f6c62)
 
 ExternalProject_Add(
   boringssl
   
   DOWNLOAD_COMMAND 
     COMMAND git init boringssl
-    COMMAND cd boringssl && git fetch --depth=1 https://github.com/google/boringssl 83da28a68f32023fd3b95a8ae94991a07b1f6c62 && git reset --hard FETCH_HEAD
+    COMMAND cd boringssl && git fetch --depth=1 https://github.com/google/boringssl ${commit_tag} && git reset --hard FETCH_HEAD
 
   PATCH_COMMAND git apply ${patch_file} && git gc --aggressive
   PREFIX ${PROJECT_BINARY_DIR}
