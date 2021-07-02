00db708d63a7bf31f5985a16f1d2186fb20f4206
drsanta@google.com <drsanta@google.com>
2021-07-02 11:42:14 -0400

    git patch uWebsockets

--- cmake/external/uWebSockets.cmake
+++ cmake/external/uWebSockets.cmake
@@ -19,16 +19,19 @@ if(TARGET uWebSockets OR NOT DOWNLOAD_UWEBSOCKETS)
 endif()
 
 set(commit 4d94401b9c98346f9afd838556fdc7dce30561eb)
+set(patch_file 
+  ${CMAKE_CURRENT_LIST_DIR}/../../scripts/git/patches/uWebsockets/0001-fix-want-write-crash.patch)
 
 ExternalProject_Add(
   uWebSockets
 
-  DOWNLOAD_DIR ${FIREBASE_DOWNLOAD_DIR}
-  DOWNLOAD_NAME uWebSockets-${commit}.tar.gz
-  URL https://github.com/uNetworking/uWebSockets/archive/${commit}.tar.gz
+  DOWNLOAD_COMMAND 
+    COMMAND git init uWebsockets
+    COMMAND cd uWebsockets && git fetch --depth=1 https://github.com/uNetworking/uWebSockets.git ${commit_tag} && git reset --hard FETCH_HEAD
+
+  PATCH_COMMAND git apply ${patch_file} && git gc --aggressive
 
   PREFIX ${PROJECT_BINARY_DIR}
-
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
