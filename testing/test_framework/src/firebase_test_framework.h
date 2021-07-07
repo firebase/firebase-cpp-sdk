ab018604c5c936cce4a45ee6473ffca17441fe45
Jon Simantov <jsimantov@google.com>
2021-07-07 14:35:27 -0700

    Rephrase messages.

--- testing/test_framework/src/firebase_test_framework.h
+++ testing/test_framework/src/firebase_test_framework.h
@@ -183,52 +183,52 @@ namespace firebase_test_framework {
 // Helper macros to assist with RunFlakyBlock.
 // Unlike EXPECT_*, FLAKY_EXPECT_* just prints out the error and returns
 // false, which will cause RunFlakyBlock to retry.
-#define FLAKY_EXPECT_EQ(a, b)                                         \
-  {                                                                   \
-    auto a_result = (a);                                              \
-    auto b_result = (b);                                              \
-    std::stringstream a_str, b_str;                                   \
-    a_str << a_result;                                                \
-    b_str << b_result;                                                \
-    if ((a_result) != (b_result)) {                                   \
-      app_framework::LogError(                                        \
-          "%s and %s expected to be equal, but differ. first(%s) vs " \
-          "second(%s)",                                               \
-          #a, #b, a_str.str().c_str(), b_str.str().c_str());          \
-      return false;                                                   \
-    }                                                                 \
+#define FLAKY_EXPECT_EQ(a, b)                                \
+  {                                                          \
+    auto a_result = (a);                                     \
+    auto b_result = (b);                                     \
+    std::stringstream a_str, b_str;                          \
+    a_str << a_result;                                       \
+    b_str << b_result;                                       \
+    if ((a_result) != (b_result)) {                          \
+      app_framework::LogError(                               \
+          "Expected %s and %s to be equal, but they differ." \
+          "first(%s) vs second(%s)",                         \
+          #a, #b, a_str.str().c_str(), b_str.str().c_str()); \
+      return false;                                          \
+    }                                                        \
   }
 
-#define FLAKY_EXPECT_NE(a, b)                                          \
-  {                                                                    \
-    auto a_result = (a);                                               \
-    auto b_result = (b);                                               \
-    std::stringstream a_str, b_str;                                    \
-    a_str << a_result;                                                 \
-    b_str << b_result;                                                 \
-    if ((a_result) == (b_result)) {                                    \
-      app_framework::LogError(                                         \
-          "%s and %s expected to differ, but are equal. first(%s) vs " \
-          "second(%s)",                                                \
-          #a, #b, a_str.str().c_str(), b_str.str().c_str());           \
-      return false;                                                    \
-    }                                                                  \
+#define FLAKY_EXPECT_NE(a, b)                                               \
+  {                                                                         \
+    auto a_result = (a);                                                    \
+    auto b_result = (b);                                                    \
+    std::stringstream a_str, b_str;                                         \
+    a_str << a_result;                                                      \
+    b_str << b_result;                                                      \
+    if ((a_result) == (b_result)) {                                         \
+      app_framework::LogError(                                              \
+          "Expected %s and %s to differ, but they are equal. first(%s) vs " \
+          "second(%s)",                                                     \
+          #a, #b, a_str.str().c_str(), b_str.str().c_str());                \
+      return false;                                                         \
+    }                                                                       \
   }
 
-#define FLAKY_EXPECT_TRUE(a)                                                 \
-  {                                                                          \
-    if (!(a)) {                                                              \
-      app_framework::LogError("%s expected to be true, but was false.", #a); \
-      return false;                                                          \
-    }                                                                        \
+#define FLAKY_EXPECT_TRUE(a)                                                   \
+  {                                                                            \
+    if (!(a)) {                                                                \
+      app_framework::LogError("Expected %s to be true, but it is false.", #a); \
+      return false;                                                            \
+    }                                                                          \
   }
 
-#define FLAKY_EXPECT_FALSE(a)                                                \
-  {                                                                          \
-    if ((a)) {                                                               \
-      app_framework::LogError("%s expected to be false, but was true.", #a); \
-      return false;                                                          \
-    }                                                                        \
+#define FLAKY_EXPECT_FALSE(a)                                                  \
+  {                                                                            \
+    if ((a)) {                                                                 \
+      app_framework::LogError("Expected %s to be false, but it is true.", #a); \
+      return false;                                                            \
+    }                                                                          \
   }
 
 #define FLAKY_WAIT_FOR_COMPLETION(future, name)                            \
