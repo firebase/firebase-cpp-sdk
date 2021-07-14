47184b783c2bfc659ef95f2f0f730793d5246df2
Konstantin Varlamov <varconst@google.com>
2021-07-14 11:45:37 -0400

    Initial commit

---
+++ firestore/src/main/firestore_exceptions_main.h
@@ -0,0 +1,8 @@
+#ifndef FIRESTORE_SRC_MAIN_FIRESTORE_EXCEPTIONS_MAIN_H_
+#define FIRESTORE_SRC_MAIN_FIRESTORE_EXCEPTIONS_MAIN_H_
+
+// Note: this header only exists to simplify the Unity build (the SWIG build has
+// trouble directly including a header that comes from the iOS repo).
+#include "Firestore/core/src/util/firestore_exceptions.h"
+
+#endif  // FIRESTORE_SRC_MAIN_FIRESTORE_EXCEPTIONS_MAIN_H_
