304e16744f2c99ff6749a83c1e5907d9831fb159
Jon Simantov <jsimantov@google.com>
2021-12-01 14:28:22 -0800

    Remove extra array subscript

--- scripts/gha/print_matrix_configuration.py
+++ scripts/gha/print_matrix_configuration.py
@@ -112,7 +112,7 @@ PARAMETERS = {
       "msvc_runtime": ["dynamic"],
       "cpp_compiler_windows": ["VisualStudio2019"],
       "cpp_compiler_linux": ["clang-11.0"],
-      "xcode_version": ["12.4"],
+      "xcode_version": ["12.4"],  # only the first one is used
       "ndk_version": ["r22b"],
       "platform_version": ["28"],
       "build_tools_version": ["28.0.3"],
