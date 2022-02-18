47ed858ad04b3d67127725fb2cd43e4e2eb768dc
Wu-Hui <wu.hui.github@gmail.com>
2022-02-18 10:31:11 -0500

    Get Firestore C++ Core from TIP by default

--- cmake/external/firestore.cmake
+++ cmake/external/firestore.cmake
@@ -18,18 +18,57 @@ if(TARGET firestore)
   return()
 endif()
 
-set(version CocoaPods-8.12.1)
-ExternalProject_Add(
-  firestore
+function(GetReleasedDep)
+  set(version CocoaPods-8.12.1)
+  message("Getting released firebase-ios-sdk @ ${version}")
+  ExternalProject_Add(
+    firestore
 
-  DOWNLOAD_DIR ${FIREBASE_DOWNLOAD_DIR}
-  URL https://github.com/firebase/firebase-ios-sdk/archive/${version}.tar.gz
+    DOWNLOAD_DIR ${FIREBASE_DOWNLOAD_DIR}
+    URL https://github.com/firebase/firebase-ios-sdk/archive/${version}.tar.gz
 
-  PREFIX ${PROJECT_BINARY_DIR}
+    PREFIX ${PROJECT_BINARY_DIR}
 
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND ""
-  INSTALL_COMMAND ""
-  TEST_COMMAND ""
-  HTTP_HEADER "${EXTERNAL_PROJECT_HTTP_HEADER}"
-)
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    TEST_COMMAND ""
+    HTTP_HEADER "${EXTERNAL_PROJECT_HTTP_HEADER}"
+    )
+endfunction()
+
+function(GetTag t)
+  message("Getting firebase-ios-sdk from ${t}")
+  ExternalProject_Add(
+    firestore
+
+    DOWNLOAD_DIR ${FIREBASE_DOWNLOAD_DIR}
+    GIT_REPOSITORY "https://github.com/firebase/firebase-ios-sdk.git"
+    GIT_TAG ${t}
+    GIT_SHALLOW "ON"
+
+    PREFIX ${PROJECT_BINARY_DIR}
+
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    TEST_COMMAND ""
+    HTTP_HEADER "${EXTERNAL_PROJECT_HTTP_HEADER}"
+    )
+endfunction()
+
+if(NOT FIRESTORE_DEP_SOURCE)
+  # Get from tip of the tree by default
+  GetTag("origin/main")
+else()
+  if(FIRESTORE_DEP_SOURCE STREQUAL "RELEASED")
+    # Use the released SDK as dependency, exact version is specified in
+    # "cmake/external/firestore.cmake".
+    GetReleasedDep()
+  elseif(FIRESTORE_DEP_SOURCE STREQUAL "TIP")
+    GetTag("origin/main")
+  else()
+    GetTag(${FIRESTORE_DEP_SOURCE})
+  endif()
+endif()
+
