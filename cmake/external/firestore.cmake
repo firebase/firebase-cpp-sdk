dcb0b3b3da62b04dc869b0b40508c82238e2516b
wu-hui <53845758+wu-hui@users.noreply.github.com>
2022-02-26 09:45:48 -0500

    (Desktop) Firestore C++ Core from TIP by default (#855)
    * Get Firestore C++ Core from TIP by default
    
    * More readable
    
    * No Origin
    
    * Master!
    
    * Keep default dependency unchanged.

--- cmake/external/firestore.cmake
+++ cmake/external/firestore.cmake
@@ -18,18 +18,52 @@ if(TARGET firestore)
   return()
 endif()
 
-set(version CocoaPods-8.12.1)
-ExternalProject_Add(
-  firestore
+function(GetReleasedDep version)
+  message("Getting released firebase-ios-sdk @ ${version}")
+  ExternalProject_Add(
+    firestore
 
-  DOWNLOAD_DIR ${FIREBASE_DOWNLOAD_DIR}
-  URL https://github.com/firebase/firebase-ios-sdk/archive/${version}.tar.gz
+    DOWNLOAD_DIR ${FIREBASE_DOWNLOAD_DIR}
+    URL https://github.com/firebase/firebase-ios-sdk/archive/${version}.tar.gz
 
-  PREFIX ${PROJECT_BINARY_DIR}
+    PREFIX ${PROJECT_BINARY_DIR}
 
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND ""
-  INSTALL_COMMAND ""
-  TEST_COMMAND ""
-  HTTP_HEADER "${EXTERNAL_PROJECT_HTTP_HEADER}"
-)
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    TEST_COMMAND ""
+    HTTP_HEADER "${EXTERNAL_PROJECT_HTTP_HEADER}"
+    )
+endfunction()
+
+function(GetTag t)
+  message("Getting firebase-ios-sdk from ${t}")
+  ExternalProject_Add(
+    firestore
+
+    DOWNLOAD_DIR ${FIREBASE_DOWNLOAD_DIR}
+    GIT_REPOSITORY "https://github.com/firebase/firebase-ios-sdk.git"
+    GIT_TAG ${t}
+    GIT_SHALLOW "ON"
+
+    PREFIX ${PROJECT_BINARY_DIR}
+
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    TEST_COMMAND ""
+    HTTP_HEADER "${EXTERNAL_PROJECT_HTTP_HEADER}"
+    )
+endfunction()
+
+if((NOT FIRESTORE_DEP_SOURCE) OR (FIRESTORE_DEP_SOURCE STREQUAL "RELEASED"))
+  # Get from released dependency by default
+  GetReleasedDep("CocoaPods-8.12.1")
+else()
+  if(FIRESTORE_DEP_SOURCE STREQUAL "TIP")
+    GetTag("origin/master")
+  else()
+    GetTag(${FIRESTORE_DEP_SOURCE})
+  endif()
+endif()
+
