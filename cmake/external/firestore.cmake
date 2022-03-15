7d2d6112b0a4df2876499e3059c4083ec747e145
Denver Coneybeare <dconeybe@google.com>
2022-03-15 00:11:07 -0400

    firestore.cmake: set custom sha1

--- cmake/external/firestore.cmake
+++ cmake/external/firestore.cmake
@@ -18,52 +18,18 @@ if(TARGET firestore)
   return()
 endif()
 
-function(GetReleasedDep version)
-  message("Getting released firebase-ios-sdk @ ${version}")
-  ExternalProject_Add(
-    firestore
+ExternalProject_Add(
+  firestore
 
-    DOWNLOAD_DIR ${FIREBASE_DOWNLOAD_DIR}
-    URL https://github.com/firebase/firebase-ios-sdk/archive/${version}.tar.gz
+  DOWNLOAD_DIR ${FIREBASE_DOWNLOAD_DIR}
+  PREFIX ${PROJECT_BINARY_DIR}
 
-    PREFIX ${PROJECT_BINARY_DIR}
+  GIT_REPOSITORY https://github.com/firebase/firebase-ios-sdk
+  GIT_TAG 8b4c02ed8d857d466e65044640e93cf6f2df70de
 
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ""
-    TEST_COMMAND ""
-    HTTP_HEADER "${EXTERNAL_PROJECT_HTTP_HEADER}"
-    )
-endfunction()
-
-function(GetTag t)
-  message("Getting firebase-ios-sdk from ${t}")
-  ExternalProject_Add(
-    firestore
-
-    DOWNLOAD_DIR ${FIREBASE_DOWNLOAD_DIR}
-    GIT_REPOSITORY "https://github.com/firebase/firebase-ios-sdk.git"
-    GIT_TAG ${t}
-    GIT_SHALLOW "ON"
-
-    PREFIX ${PROJECT_BINARY_DIR}
-
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ""
-    TEST_COMMAND ""
-    HTTP_HEADER "${EXTERNAL_PROJECT_HTTP_HEADER}"
-    )
-endfunction()
-
-if((NOT FIRESTORE_DEP_SOURCE) OR (FIRESTORE_DEP_SOURCE STREQUAL "RELEASED"))
-  # Get from released dependency by default
-  GetReleasedDep("CocoaPods-8.12.1")
-else()
-  if(FIRESTORE_DEP_SOURCE STREQUAL "TIP")
-    GetTag("origin/master")
-  else()
-    GetTag(${FIRESTORE_DEP_SOURCE})
-  endif()
-endif()
-
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+  TEST_COMMAND ""
+  HTTP_HEADER "${EXTERNAL_PROJECT_HTTP_HEADER}"
+)
