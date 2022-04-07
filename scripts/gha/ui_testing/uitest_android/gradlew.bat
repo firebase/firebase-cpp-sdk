44b97138ab5ab477154342325c631becce6e2b9e
Mou <sunmou@google.com>
2022-04-07 13:58:34 -0700

    update based on comments

--- scripts/gha/ui_testing/uitest_android/gradlew.bat
+++ scripts/gha/ui_testing/uitest_android/gradlew.bat
@@ -1,4 +1,4 @@
-@rem Copyright 2021 Google LLC
+@rem Copyright 2022 Google LLC
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License"); 
 @rem you may not use this file except in compliance with the License. 
