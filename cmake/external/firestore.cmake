96e26beb0ec5cdd0f1dd65b083afb56329d023f8
Jon Simantov <jsimantov@google.com>
2022-04-25 12:30:22 -0700

    Enable Firestore and Admob.

--- cmake/external/firestore.cmake
+++ cmake/external/firestore.cmake
@@ -20,7 +20,7 @@ endif()
 
 # If the format of the line below changes, then be sure to update
 # https://github.com/firebase/firebase-cpp-sdk/blob/fd054fa016/.github/workflows/update-dependencies.yml#L81
-set(version CocoaPods-8.15.0)
+set(version CocoaPods-9.0.0-nightly)
 
 function(GetReleasedDep)
   message("Getting released firebase-ios-sdk @ ${version}")
