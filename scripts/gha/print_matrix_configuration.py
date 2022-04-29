28672b78a15fee5dc3ae512b170a4c6b02693899
a-maurice <amaurice@google.com>
2022-04-29 13:37:14 -0700

    Update the testing Xcode version to 13.3.1

--- scripts/gha/print_matrix_configuration.py
+++ scripts/gha/print_matrix_configuration.py
@@ -76,12 +76,12 @@ PARAMETERS = {
       "build_type": ["Release", "Debug"],
       "architecture": ["x64", "x86", "arm64"],
       "msvc_runtime": ["static","dynamic"],
-      "xcode_version": ["13.2.1"],
+      "xcode_version": ["13.3.1"],
       "python_version": ["3.7"],
 
       EXPANDED_KEY: {
         "os": ["ubuntu-latest", "macos-latest", "windows-latest"],
-        "xcode_version": ["13.2.1"],
+        "xcode_version": ["13.3.1"],
       }
     }
   },
@@ -112,7 +112,7 @@ PARAMETERS = {
       "msvc_runtime": ["dynamic"],
       "cpp_compiler_windows": ["VisualStudio2019"],
       "cpp_compiler_linux": ["clang-11.0"],
-      "xcode_version": ["13.2.1"],  # only the first one is used
+      "xcode_version": ["13.3.1"],  # only the first one is used
       "ndk_version": ["r22b"],
       "platform_version": ["28"],
       "build_tools_version": ["28.0.3"],
@@ -138,10 +138,10 @@ PARAMETERS = {
 
   "ios": {
     "matrix": {
-      "xcode_version": ["13.2.1"],
+      "xcode_version": ["13.3.1"],
 
       EXPANDED_KEY: {
-        "xcode_version": ["13.2.1"]
+        "xcode_version": ["13.3.1"]
       }
     }
   },
