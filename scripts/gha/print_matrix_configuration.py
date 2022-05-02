65659bd917512225968d3dfbfdaf4d0a98804e1f
Jon Simantov <jsimantov@google.com>
2022-05-02 11:59:29 -0700

    Test on simulator 15.0

--- scripts/gha/print_matrix_configuration.py
+++ scripts/gha/print_matrix_configuration.py
@@ -170,8 +170,8 @@ TEST_DEVICES = {
   "ios_min": {"type": "real", "model":"iphone8", "version":"11.4"},
   "ios_target": {"type": "real", "model":"iphone8plus", "version":"12.0"},
   "ios_latest": {"type": "real", "model":"iphone11pro", "version":"14.7"},
-  "simulator_min": {"type": "virtual", "name":"iPhone 6", "version":"15.4"},
-  "simulator_target": {"type": "virtual", "name":"iPhone 8", "version":"15.4"},
+  "simulator_min": {"type": "virtual", "name":"iPhone 6", "version":"15.0"},
+  "simulator_target": {"type": "virtual", "name":"iPhone 8", "version":"15.0"},
   "simulator_latest": {"type": "virtual", "name":"iPhone 11", "version":"15.4"},
   "tvos_simulator": {"type": "virtual", "name":"Apple TV", "version":"15.0"},
 }
