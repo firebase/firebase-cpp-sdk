3c3602b227cb6b971269bb4064ce4c5df6d157ee
Jon Simantov <jsimantov@google.com>
2022-06-02 12:36:03 -0700

    Remove extra namespaces - they are underneath re2::

--- build_scripts/desktop/package.sh
+++ build_scripts/desktop/package.sh
@@ -229,7 +229,7 @@ readonly -a rename_namespaces=(flatbuffers flexbuffers reflection ZLib bssl uWS 
                                grpc_ssl_server_credentials grpc_tls_credential_reload_config
                                grpc_tls_server_authorization_check_config GrpcUdpListener leveldb
                                leveldb_filterpolicy_create_bloom leveldb_writebatch_iterate strings
-                               TlsCredentials TlsServerCredentials tsi snappy re2 re2_internal hooks)
+                               TlsCredentials TlsServerCredentials tsi snappy re2)
 
 # String to prepend to all hidden symbols.
 readonly rename_string=f_b_
