169d9665e2965c8682aa22853dedfe86e3a3dc57
a-maurice <amaurice@google.com>
2022-09-22 15:58:18 -0700

    Update utils.py

--- scripts/gha/utils.py
+++ scripts/gha/utils.py
@@ -232,13 +232,6 @@ def install_x86_support_libraries(gha_build=False):
     packages = ['gcc-multilib', 'g++-multilib', 'libglib2.0-dev:i386',
                 'libsecret-1-dev:i386', 'libpthread-stubs0-dev:i386',
                 'libssl-dev:i386']
-    #if gha_build:
-      # Workaround for GitHub runners, which have an incompatibility between the
-      # 64-bit and 32-bit versions of the Ubuntu package libpcre2-8-0. Downgrade
-      # the installed 64-bit version of the library to get around this issue.
-      # This will presumably be fixed in a future Ubuntu update. (If you remove
-      # it, remove the workaround further down this function as well.)
-      #packages = ['--allow-downgrades'] + packages + ['libpcre2-8-0=10.34-7']
 
     # First check if these packages exist on the machine already
     with open(os.devnull, "w") as devnull:
@@ -251,13 +244,3 @@ def install_x86_support_libraries(gha_build=False):
       run_command(['apt', 'update'], as_root=True, check=True)
       run_command(['apt', 'install', 'aptitude'], as_root=True, check=True)
       run_command(['aptitude', 'install', '-V', '-y'] + packages, as_root=True, check=True)
-
-    #if gha_build:
-      # One more workaround: downgrading libpcre2-8-0 above may have uninstalled
-      # libsecret, which is required for the Linux build. Force it to be
-      # reinstalled, but do it as a separate command to ensure that held
-      # packages aren't modified. (Once the workaround above is removed, this can
-      # be removed as well.)
-      # Note: "-f" = "fix" - let apt do what it needs to do to fix dependencies.
-      #run_command(['apt', 'install', '-f', '-V', '-y', 'libsecret-1-dev'],
-      #            as_root=True, check=True)
