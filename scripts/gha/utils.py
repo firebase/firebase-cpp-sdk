da1e738974e8cf4bc11d71712311facf4cb3a5a2
a-maurice <amaurice@google.com>
2022-09-22 15:14:21 -0700

    Update utils.py

--- scripts/gha/utils.py
+++ scripts/gha/utils.py
@@ -249,6 +249,7 @@ def install_x86_support_libraries(gha_build=False):
       # Install them.
       run_command(['dpkg', '--add-architecture', 'i386'], as_root=True, check=True)
       run_command(['apt', 'update'], as_root=True, check=True)
+      run_command(['apt', 'install', 'aptitude'], as_root=True, check=True)
       run_command(['aptitude', 'install', '-V', '-y'] + packages, as_root=True, check=True)
 
     if gha_build:
