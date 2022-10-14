3bfffc0569ce63c51bf716ceed38848604f8e3fc
drsanta@google.com <drsanta@google.com>
2022-10-14 14:21:59 -0400

    ensure aptitude is installed

--- scripts/gha/utils.py
+++ scripts/gha/utils.py
@@ -228,20 +228,22 @@ def install_x86_support_libraries(gha_build=False):
                workarounds that might be undesirable on a personal system (e.g.
                downgrading Ubuntu packages).
   """
+  print("install_x86_support_libraries start")
   if is_linux_os():
+    print("install_x86_support_libraries is_linux_os")
     packages = ['gcc-multilib', 'g++-multilib', 'libglib2.0-dev:i386',
                 'libsecret-1-dev:i386', 'libpthread-stubs0-dev:i386',
                 'libssl-dev:i386']
 
     # First check if these packages exist on the machine already
-    with open(os.devnull, "w") as devnull:
-      process = subprocess.run(["dpkg", "-s"] + packages, stdout=devnull, stderr=subprocess.STDOUT)
+    #with open(os.devnull, "w") as devnull:
+    #  process = subprocess.run(["dpkg", "-s"] + packages, stdout=devnull, stderr=subprocess.STDOUT)
 
-    if process.returncode != 0:
+    #if process.returncode != 0:
       # This implies not all of the required packages are already installed on user's machine
       # Install them.
-      run_command(['dpkg', '--add-architecture', 'i386'], as_root=True, check=True)
-      run_command(['apt', 'update'], as_root=True, check=True)
-      run_command(['apt', 'install', 'aptitude'], as_root=True, check=True)
-      for package in packages:
-        run_command(['aptitude', 'install', '-V', '-y'] + package, as_root=True, check=True)
+    run_command(['dpkg', '--add-architecture', 'i386'], as_root=True, check=True)
+    run_command(['apt', 'update'], as_root=True, check=True)
+    run_command(['apt', 'install', 'aptitude'], as_root=True, check=True)
+    for package in packages:
+      run_command(['aptitude', 'install', '-V', '-y'] + package, as_root=True, check=True)
