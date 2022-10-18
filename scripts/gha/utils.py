8dcb1e6d078123a022c088aee6d03fb536f9f530
drsanta@google.com <drsanta@google.com>
2022-10-18 10:00:48 -0400

    loop packages

--- scripts/gha/utils.py
+++ scripts/gha/utils.py
@@ -245,4 +245,6 @@ def install_x86_support_libraries(gha_build=False):
       # Install them.
       run_command(['apt', 'update'], as_root=True, check=True)
       run_command(['apt', 'install', 'aptitude'], as_root=True, check=True)
-      run_command(['aptitude', 'install', '-V', '-y'] + packages, as_root=True, check=True)
+      for package in packages:
+        run_command(['aptitude', 'install', '-V', '-y', package], as_root=True, check=True)
+
