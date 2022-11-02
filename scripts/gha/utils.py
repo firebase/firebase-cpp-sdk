1a326b12891a01e33c2fdfb0bd538103bca6f012
Jon Simantov <jsimantov@gmail.com>
2022-11-02 19:52:50 +0000

    Add a check after installing dependencies to verify they installed.

--- scripts/gha/utils.py
+++ scripts/gha/utils.py
@@ -236,12 +236,14 @@ def install_x86_support_libraries(gha_build=False):
 
     # First check if these packages exist on the machine already
     with open(os.devnull, "w") as devnull:
-      process = subprocess.run(["dpkg", "-s"] + packages, stdout=devnull, stderr=subprocess.STDOUT)
+      process = subprocess.run(["dpkg", "-s"] + packages, stdout=devnull,
+                               stderr=subprocess.STDOUT)
 
     if process.returncode != 0:
-      # This implies not all of the required packages are already installed on user's machine
-      # Install them.
-      run_command(['dpkg', '--add-architecture', 'i386'], as_root=True, check=True)
+      # This implies not all of the required packages are already installed on
+      # user's machine Install them.
+      run_command(['dpkg', '--add-architecture', 'i386'], as_root=True,
+                  check=True)
       run_command(['apt', 'update'], as_root=True, check=True)
       run_command(['apt', 'install', 'aptitude'], as_root=True, check=True)
       if gha_build:
@@ -250,13 +252,16 @@ def install_x86_support_libraries(gha_build=False):
         remove_packages = ['libpcre2-dev:amd64', 'libpcre2-32-0:amd64',
                            'libpcre2-8-0:amd64', 'libpcre2-16-0:amd64',
                            'zlib1g:amd64', 'zlib1g-dev:amd64']
-
+      # Note: With aptitude, you can remove package 'xyz' by specifying 'xyz-'
+      # in the package list.
       run_command(['aptitude', 'install', '-V', '-y'] + packages +
                   ['%s-' % pkg for pkg in remove_packages], as_root=True, check=True)
 
-      # Debug: Show that libsecret was installed.
+      # Debug: Show that libsecret is installed.
       run_command(['dpkg', '-l', '*libsecret*'])
+
       # Check if the packages were installed
       with open(os.devnull, "w") as devnull:
-        subprocess.run(["dpkg", "-s"] + packages, stdout=devnull, stderr=subprocess.STDOUT, check=True)
+        subprocess.run(["dpkg", "-s"] + packages, stdout=devnull, stderr=subprocess.STDOUT
+                       check=True)
 
