3d715e2a9ef9a8c2ca37eee81dbb6456f8007b4e
Jon Simantov <jsimantov@google.com>
2022-11-03 16:36:40 -0700

    Add mutex to completion handler on iOS.

--- analytics/src/analytics_android.cc
+++ analytics/src/analytics_android.cc
@@ -421,7 +421,8 @@ Future<int64_t> GetSessionId() {
         reinterpret_cast<void*>(future_handle.get().id()),
         internal::kAnalyticsModuleName);
   } else {
-    api->CompleteWithResult(future_handle.get(), -1, error.c_str(), static_cast<int64_t>(0L));
+    api->CompleteWithResult(future_handle.get(), -1, error.c_str(),
+                            static_cast<int64_t>(0L));
   }
   env->DeleteLocalRef(task);
 
