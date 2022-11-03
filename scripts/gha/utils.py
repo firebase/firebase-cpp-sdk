0505dbb704d491bdd400ca9d72d619680d069c25
Jon Simantov <jsimantov@google.com>
2022-11-03 14:24:40 -0700

    Fix toolchain selection and packages on linux x86 (#1133)
    * Fix toolchain selection on linux.
    
    * Add path to 32-bit packages to pkgconfig environment variable.
    
    * Add extra libsecret package.
    
    * Remove conflicting packages
    
    * Remove and add at the same time.
    
    * Add a check after installing dependencies to verify they installed.
    
    * Write package config path to temp toolchain file.
    
    * Add readme.

--- scripts/gha/utils.py
+++ scripts/gha/utils.py
@@ -231,16 +231,33 @@ def install_x86_support_libraries(gha_build=False):
   if is_linux_os():
     packages = ['gcc-multilib', 'g++-multilib', 'libglib2.0-dev:i386',
                 'libsecret-1-dev:i386', 'libpthread-stubs0-dev:i386',
-                'libssl-dev:i386']
+                'libssl-dev:i386', 'libsecret-1-0:i386']
+    remove_packages = []
 
     # First check if these packages exist on the machine already
     with open(os.devnull, "w") as devnull:
-      process = subprocess.run(["dpkg", "-s"] + packages, stdout=devnull, stderr=subprocess.STDOUT)
+      process = subprocess.run(["dpkg", "-s"] + packages, stdout=devnull,
+                               stderr=subprocess.STDOUT)
 
     if process.returncode != 0:
-      # This implies not all of the required packages are already installed on user's machine
-      # Install them.
-      run_command(['dpkg', '--add-architecture', 'i386'], as_root=True, check=True)
+      # This implies not all of the required packages are already installed on
+      # user's machine. Install them.
+      run_command(['dpkg', '--add-architecture', 'i386'], as_root=True,
+                  check=True)
       run_command(['apt', 'update'], as_root=True, check=True)
       run_command(['apt', 'install', 'aptitude'], as_root=True, check=True)
-      run_command(['aptitude', 'install', '-V', '-y'] + packages, as_root=True, check=True)
+      if gha_build:
+        # Remove libpcre to prevent package conflicts.
+        # Only remove packages on GitHub runners.
+        remove_packages = ['libpcre2-dev:amd64', 'libpcre2-32-0:amd64',
+                           'libpcre2-8-0:amd64', 'libpcre2-16-0:amd64']
+      # Note: With aptitude, you can remove package 'xyz' by specifying 'xyz-'
+      # in the package list.
+      run_command(['aptitude', 'install', '-V', '-y'] + packages +
+                  ['%s-' % pkg for pkg in remove_packages], as_root=True, check=True)
+
+      # Check if the packages were installed
+      with open(os.devnull, "w") as devnull:
+        subprocess.run(["dpkg", "-s"] + packages, stdout=devnull, stderr=subprocess.STDOUT,
+                       check=True)
+
