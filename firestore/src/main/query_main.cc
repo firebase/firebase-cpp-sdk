7de73ba40292277546fce496e6a40b5c1f2ef80c
a-maurice <amaurice@google.com>
2022-11-07 12:16:33 -0800

    Pin to a version of iOS with nanopb fix (#1136)
    * Pin to a version of iOS with nanopb fix
    
    * Update query_main.cc
    
    * Update query_main.cc
    
    * Update query_main.cc
    
    * Update query_main.cc

--- firestore/src/main/query_main.cc
+++ firestore/src/main/query_main.cc
@@ -237,8 +237,13 @@ core::Bound QueryInternal::ToBound(
     const std::vector<FieldValue>& field_values) const {
   const core::Query& internal_query = query_.query();
   // Use explicit order bys  because it has to match the query the user made.
+#if TARGET_OS_IOS || TARGET_OS_TV
   const core::OrderByList& explicit_order_bys =
       internal_query.explicit_order_bys();
+#else
+  const std::vector<core::OrderBy>& explicit_order_bys =
+      internal_query.explicit_order_bys();
+#endif
 
   if (field_values.size() > explicit_order_bys.size()) {
     SimpleThrowInvalidArgument(
