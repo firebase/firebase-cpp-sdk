1fc97710d981e312e0c1067075c2dfde920b67de
Cynthia Jiang <cynthiajiang@google.com>
2022-11-17 13:24:51 -0800

    undo format

--- build_scripts/desktop/finish_darwin.sh
+++ build_scripts/desktop/finish_darwin.sh
@@ -1,33 +1,40 @@
-#!/ bin / bash
+#!/bin/bash
 
-#Copyright 2020 Google LLC
+# Copyright 2020 Google LLC
 #
-#Script to finish packaging individual iOS / Mac libraries into
-#universal libraries, and then into frameworks.
-set - e
+# Script to finish packaging individual iOS/Mac libraries into
+# universal libraries, and then into frameworks.
+set -e
 
-    if[["$(uname)" != "Darwin"]];
-then echo
-    "Error: Frameworks and universal libraries can only be built on a Mac "
-    "host." exit 2 fi
+if [[ "$(uname)" != "Darwin" ]]; then
+    echo "Error: Frameworks and universal libraries can only be built on a Mac host."
+    exit 2
+fi
 
-        packagepath = $1 includepath = $2 binutils = $3 os = $4
+packagepath=$1
+includepath=$2
+binutils=$3
+os=$4
 
-    if[[-z "${os}"]];
-then os = darwin fi
+if [[ -z "${os}" ]]; then
+    os=darwin
+fi
 
-    if[[-z "${packagepath}" || -z "${includepath}" || -z "${binutils}"]];
-then echo
-    "Usage: $0 <packaged iOS/Mac SDK path> <include file path> <binutils path> "
-    "[darwin|ios]" exit 2 fi
+if [[ -z "${packagepath}" || -z "${includepath}" || -z "${binutils}" ]]; then
+    echo "Usage: $0 <packaged iOS/Mac SDK path> <include file path> <binutils path> [darwin|ios]"
+    exit 2
+fi
 
-    if[[!-d "${packagepath}/libs/${os}"]];
-then echo "Can't find packaged iOS/Mac SDK in ${packagedpath}/libs/${os}" fi
+if [[ ! -d "${packagepath}/libs/${os}" ]]; then
+    echo "Can't find packaged iOS/Mac SDK in ${packagedpath}/libs/${os}"
+fi
 
-    if[[!-r "${includepath}/firebase/app.h"]];
-then echo "Can't find include files in ${includepath}" exit 2 fi
+if [[ ! -r "${includepath}/firebase/app.h" ]]; then
+    echo "Can't find include files in ${includepath}"
+    exit 2
+fi
 
-    if[[!-x "${binutils}/ar"]]; then
+if [[ ! -x "${binutils}/ar" ]]; then
     echo "Packaging tools not found at path '${binutils}'."
     exit 2
 fi
@@ -38,47 +45,41 @@ includepath=$(cd ${includepath}; pwd -P)
 binutils=$(cd ${binutils}; pwd -P)
 
 declare -a architectures
-for a in $(cd "${packagepath}"/libs/"${os}"; find . -depth 1 -type d | sed 's|^\./||');
-do
-  if
-    [[$a != "universal"]];
-then architectures += ($a)fi done
+for a in $(cd "${packagepath}"/libs/"${os}"; find . -depth 1 -type d | sed 's|^\./||'); do
+    if [[ $a != "universal" ]]; then
+	architectures+=($a)
+    fi
+done
 
-#Don't lipo if we already have universal libraries, as that
-#means that the lipo step has already occurred.
-    if[[!-d "${packagepath}/libs/${os}/universal"]]; then
+# Don't lipo if we already have universal libraries, as that
+# means that the lipo step has already occurred.
+if [[ ! -d "${packagepath}/libs/${os}/universal" ]]; then
     echo "Repackaging libraries using Mac libtool..."
-    for arch in ${architectures[*]};
-do
-        for
-lib in
-    "${packagepath}/libs/${os}/${arch}" /*.a; do
-pushd $(dirname ${lib}) > /dev/null
-libname=$(basename "${lib}")
-mkdir "${libname}.dir"
-cd "${libname}.dir"
-"${binutils}"/ar x ../"${libname}"
-mv -f ../"${libname}"  ../"${libname}.bak"
-xcrun libtool -static -o "../${libname}" -no_warning_for_no_symbols -s *.o
-cd ..
-rm -rf "${libname}.dir"
-popd > /dev/null
-done
-done
+    for arch in ${architectures[*]}; do
+	for lib in "${packagepath}/libs/${os}/${arch}"/*.a; do
+	    pushd $(dirname ${lib}) > /dev/null
+	    libname=$(basename "${lib}")
+	    mkdir "${libname}.dir"
+	    cd "${libname}.dir"
+	    "${binutils}"/ar x ../"${libname}"
+	    mv -f ../"${libname}"  ../"${libname}.bak"
+	    xcrun libtool -static -o "../${libname}" -no_warning_for_no_symbols -s *.o
+	    cd ..
+	    rm -rf "${libname}.dir"
+	    popd > /dev/null
+	done
+    done
 
-# Merge into universal libs
-mkdir "${packagepath}/libs/${os}/universal"
-echo "Creating universal libraries..."
-for lib in "${packagepath}/libs/${os}/${architectures[0]}"/*.a; do
-libname=$(basename ${lib})
-xcrun lipo "${packagepath}/libs/${os}"/*/
-    "${libname}" -
-    create -
-    output "${packagepath}/libs/${os}/universal/${libname}" done
+    # Merge into universal libs
+    mkdir "${packagepath}/libs/${os}/universal"
+    echo "Creating universal libraries..."
+    for lib in "${packagepath}/libs/${os}/${architectures[0]}"/*.a; do
+		libname=$(basename ${lib})
+		xcrun lipo "${packagepath}/libs/${os}"/*/"${libname}" -create -output "${packagepath}/libs/${os}/universal/${libname}"
+    done
 
-#Done, remove old library backups.
-        rm -
-    f "${packagepath}/libs/${os}"/*/*.bak
+    # Done, remove old library backups.
+    rm -f "${packagepath}/libs/${os}"/*/*.bak
 
 else
     echo "Universal libraries already found, skipping..."
