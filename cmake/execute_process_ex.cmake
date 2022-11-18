58b3422b79002639c123934f71e9fcfb1b8e82a7
Denver Coneybeare <dconeybe@google.com>
2022-11-18 16:02:23 -0500

    configure the downloaded firebase-ios-sdk on android builds, just to get absl

---
+++ cmake/execute_process_ex.cmake
@@ -0,0 +1,82 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Invokes cmake's built-in execute_process() function, but also performs some
+# logging and fails cmake if the process completes with a non-zero exit code.
+#
+# Arguments:
+#   COMMAND
+#     The command to run; it is forwarded verbatim to execute_process().
+#   DESCRIPTION
+#     (optional) A description to include in log messages about the command.
+#     Example: "run the foo output through the bar filter"
+function(execute_process_ex)
+  cmake_parse_arguments(
+    PARSE_ARGV 0
+    ARG
+    "" # zero-value arguments
+    "DESCRIPTION" # single-value arguments
+    "COMMAND" # multi-value arguments
+  )
+
+  # Validate the arguments
+  list(LENGTH ARG_COMMAND ARG_COMMAND_LENGTH)
+  if(ARG_COMMAND_LENGTH EQUAL 0)
+    message(
+      FATAL_ERROR
+      "COMMAND must be specified to ${CMAKE_CURRENT_FUNCTION} "
+      "and must have at least one value (DESCRIPTION=${ARG_DESCRIPTION})"
+    )
+  endif()
+
+  # Log that the process is about to be executed.
+  set(ARG_COMMAND_STR "")
+  foreach(ARG_COMMAND_STR_COMPONENT ${ARG_COMMAND})
+    string(APPEND ARG_COMMAND_STR " ${ARG_COMMAND_STR_COMPONENT}")
+  endforeach()
+  string(SUBSTRING "${ARG_COMMAND_STR}" 1, -1 ARG_COMMAND_STR)
+  if("${ARG_DESCRIPTION}" STREQUAL "")
+    message(STATUS "Command starting: ${ARG_COMMAND_STR}")
+  else()
+    message(STATUS "Command starting (${ARG_DESCRIPTION}): ${ARG_COMMAND_STR}")
+  endif()
+
+  # Execute the process
+  execute_process(
+    COMMAND
+      ${ARG_COMMAND}
+    RESULT_VARIABLE
+      EXECUTE_PROCESS_RESULT
+  )
+
+  # Verify that the command completed successfully.
+  if(NOT (EXECUTE_PROCESS_RESULT EQUAL 0))
+    message(
+      FATAL_ERROR
+      "Command completed with non-zero exit code ${EXECUTE_PROCESS_RESULT} "
+      "(DESCRIPTION=${ARG_DESCRIPTION}): ${ARG_COMMAND_STR}"
+    )
+  endif()
+
+  # Log the success of the command.
+  if("${ARG_DESCRIPTION}" STREQUAL "")
+    message(STATUS "Command completed successfully: ${ARG_COMMAND_STR}")
+  else()
+    message(
+      STATUS
+      "Command completed successfully (${ARG_DESCRIPTION}): ${ARG_COMMAND_STR}"
+    )
+  endif()
+
+endfunction(execute_process_ex)
