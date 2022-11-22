33468f2f256f4000fe1d3ade2cc28ef0c4eacf88
Jon Simantov <jsimantov@google.com>
2022-11-21 16:50:25 -0800

    Add missing package specifier.

--- testing/test_framework/src/android/java/com/google/firebase/example/TestHelper.java
+++ testing/test_framework/src/android/java/com/google/firebase/example/TestHelper.java
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+package com.google.firebase.example;
+
 import android.content.Context;
 import android.net.wifi.WifiManager;
 import android.os.Build;
