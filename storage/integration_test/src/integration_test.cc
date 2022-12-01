6b7902790669c0296176f16aace7ae6548714352
Jon Simantov <jsimantov@google.com>
2022-12-01 11:04:58 -0800

    Skip a test that's flaky on Android emulator.

--- storage/integration_test/src/integration_test.cc
+++ storage/integration_test/src/integration_test.cc
@@ -399,8 +399,10 @@ const std::string kSimpleTestFile =
     "culpa qui officia deserunt mollit anim id est laborum.";
 
 TEST_F(FirebaseStorageTest, TestWriteAndReadByteBuffer) {
-  SignIn();
+  SKIP_TEST_ON_ANDROID_EMULATOR();
 
+  SignIn();
+  
   firebase::storage::StorageReference ref =
       CreateFolder().Child("TestFile.txt");
   LogDebug("Storage URL: gs://%s%s", ref.bucket().c_str(),
