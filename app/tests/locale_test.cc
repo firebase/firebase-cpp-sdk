18bb49476ede8f15fcfe74a4c9f7334655de3f04
Jon Simantov <jsimantov@google.com>
2023-07-05 14:13:13 -0700

    Format code 2

--- app/tests/locale_test.cc
+++ app/tests/locale_test.cc
@@ -64,10 +64,10 @@ TEST_F(LocaleTest, TestConvertingStringEncodings) {
 
   // "Mitteleuropäische Zeit €" in UTF-16 encoding
   const wchar_t original_utf16_array[] = {
-      'M',    'i', 't',    't', 'e',    'l',    'e',   'u',    'r',
-      'o',    'p', 0x00E4, 'i', 's',    'c',    'h',   'e',    ' ',
-      'Z',    'e', 'i',    't', ' ',    0x20AC, ' ',   'd',    '\'',
-      0x00E9, 't', 0x00E9, ' ', 0x2018, 0x17D,  0x153, 0x2019, '\0'};
+      'M',    'i', 't',    't', 'e',    'l',     'e',    'u',    'r',
+      'o',    'p', 0x00E4, 'i', 's',    'c',     'h',    'e',    ' ',
+      'Z',    'e', 'i',    't', ' ',    0x20AC,  ' ',    'd',    '\'',
+      0x00E9, 't', 0x00E9, ' ', 0x2018, 0x017D,  0x0153, 0x2019, '\0'};
   std::wstring original_utf16(
       reinterpret_cast<const wchar_t*>(original_utf16_array));
 
