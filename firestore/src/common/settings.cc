570e8c0edb37039e7cd94b6e2f135d19123c4c2b
Wu-Hui <wu.hui.github@gmail.com>
2023-07-12 11:24:32 -0400

    remove unnecessary include

--- firestore/src/common/settings.cc
+++ firestore/src/common/settings.cc
@@ -21,7 +21,6 @@
 #include <sstream>
 #include <utility>
 
-#include "Firestore/core/src/util/hard_assert.h"
 #include "firebase/firestore/local_cache_settings.h"
 #include "firestore/src/common/exception_common.h"
 
