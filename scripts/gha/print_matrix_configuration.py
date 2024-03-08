83cc51e12de695d5b6b9c22018f54b37a59495da
Jon Simantov <jsimantov@google.com>
2024-03-08 15:27:12 -0800

    Fix iOS version for minimum simulator.

--- scripts/gha/print_matrix_configuration.py
+++ scripts/gha/print_matrix_configuration.py
@@ -222,7 +222,7 @@ TEST_DEVICES = {
       {"type": "ftl", "device": "model=iphone8,version=16.6"},
       {"type": "ftl", "device": "model=ipad10,version=16.6"},
   ],
-  "simulator_min": [ {"type": "virtual", "name":"iPhone SE (3rd generation)", "version":"17.0"} ],
+  "simulator_min": [ {"type": "virtual", "name":"iPhone SE (3rd generation)", "version":"17.0.1"} ],
   "simulator_target": [ {"type": "virtual", "name":"iPhone 15", "version":"17.2"} ],
   "simulator_latest": [ {"type": "virtual", "name":"iPad Pro (12.9-inch) (6th generation)", "version":"17.4"} ],
   "tvos_simulator": [ {"type": "virtual", "name":"Apple TV Simulator", "version":"16.1"} ],
