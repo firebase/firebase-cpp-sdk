c1b0986d2634f69159e73f52609328487f1f2ca7
Jon Simantov <jsimantov@google.com>
2024-03-08 17:48:58 -0800

    Fix extraneous "Simulator" word.

--- scripts/gha/print_matrix_configuration.py
+++ scripts/gha/print_matrix_configuration.py
@@ -225,7 +225,7 @@ TEST_DEVICES = {
   "simulator_min": [ {"type": "virtual", "name":"iPhone SE (3rd generation)", "version":"17.0.1"} ],
   "simulator_target": [ {"type": "virtual", "name":"iPhone 15", "version":"17.2"} ],
   "simulator_latest": [ {"type": "virtual", "name":"iPad Pro (12.9-inch) (6th generation)", "version":"17.4"} ],
-  "tvos_simulator": [ {"type": "virtual", "name":"Apple TV Simulator", "version":"16.1"} ],
+  "tvos_simulator": [ {"type": "virtual", "name":"Apple TV", "version":"16.1"} ],
 }
 
 # Easy accesssor for getting a TEST_DEVICES entry. Note that once a device model
