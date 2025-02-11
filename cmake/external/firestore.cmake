1d33c9b33eb7101be5d50b222827ece5c15003fb
Jon Simantov <jsimantov@google.com>
2025-02-10 16:45:38 -0800

    Use a temporary branch for Firestore SDK.

--- cmake/external/firestore.cmake
+++ cmake/external/firestore.cmake
@@ -20,7 +20,8 @@ endif()
 
 # If the format of the line below changes, then be sure to update
 # https://github.com/firebase/firebase-cpp-sdk/blob/fd054fa016/.github/workflows/update-dependencies.yml#L81
-set(version CocoaPods-11.8.1)
+#set(version CocoaPods-11.8.1)
+set(version firestore-core-android-build-fix)
 
 function(GetReleasedDep)
   message("Getting released firebase-ios-sdk @ ${version}")
