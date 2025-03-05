cdd16f7d5545a195be0e0fbe3faf6c4d83af1ed8
Jon Simantov <jsimantov@google.com>
2025-03-05 12:25:04 -0800

    Add missing libpcre2.

--- scripts/gha/utils.py
+++ scripts/gha/utils.py
@@ -237,7 +237,8 @@ def install_x86_support_libraries(gha_build=False):
   if is_linux_os():
     packages = ['gcc-multilib', 'g++-multilib', 'libglib2.0-dev:i386',
                 'libsecret-1-dev:i386', 'libpthread-stubs0-dev:i386',
-                'libssl-dev:i386', 'libsecret-1-0:i386']
+                'libssl-dev:i386', 'libsecret-1-0:i386',
+                'libpcre2-dev:i386']
     remove_packages = []
 
     # First check if these packages exist on the machine already
