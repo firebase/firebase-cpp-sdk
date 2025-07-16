77d83cec2940c3930c07c7bc7e06f9667b005746
a-maurice <amaurice@google.com>
2025-07-16 15:20:10 -0700

    Update minimum iOS deployment target to 15

--- cmake/toolchains/apple.toolchain.cmake
+++ cmake/toolchains/apple.toolchain.cmake
@@ -253,15 +253,15 @@ if(NOT DEFINED DEPLOYMENT_TARGET)
     # Unless specified, SDK version 11.0 (Big Sur) is used by default as minimum target version (macos on arm).
     set(DEPLOYMENT_TARGET "11.0")
   elseif(PLATFORM STREQUAL "MAC_CATALYST" OR PLATFORM STREQUAL "MAC_CATALYST_ARM64")
-    # Unless specified, SDK version 13.0 is used by default as minimum target version (mac catalyst minimum requirement).
-    set(DEPLOYMENT_TARGET "13.0")
+    # Unless specified, SDK version 15.0 is used by default as minimum target version (mac catalyst minimum requirement).
+    set(DEPLOYMENT_TARGET "15.0")
   else()
-    # Unless specified, SDK version 11.0 is used by default as minimum target version (iOS, tvOS).
-    set(DEPLOYMENT_TARGET "13.0")
+    # Unless specified, SDK version 15.0 is used by default as minimum target version (iOS, tvOS).
+    set(DEPLOYMENT_TARGET "15.0")
   endif()
   message(STATUS "[DEFAULTS] Using the default min-version since DEPLOYMENT_TARGET not provided!")
-elseif(DEFINED DEPLOYMENT_TARGET AND PLATFORM STREQUAL "MAC_CATALYST" AND ${DEPLOYMENT_TARGET} VERSION_LESS "13.0")
-  message(FATAL_ERROR "Mac Catalyst builds requires a minimum deployment target of 13.0!")
+elseif(DEFINED DEPLOYMENT_TARGET AND PLATFORM STREQUAL "MAC_CATALYST" AND ${DEPLOYMENT_TARGET} VERSION_LESS "15.0")
+  message(FATAL_ERROR "Mac Catalyst builds requires a minimum deployment target of 15.0!")
 endif()
 
 # Store the DEPLOYMENT_TARGET in the cache
