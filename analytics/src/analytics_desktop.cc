065c47b51ac25d4c980c19f80878c7f2c36c8e23
Austin Benoit <austinbenoit@google.com>
2025-11-21 16:24:39 -0500

    Add in future header to the integration tests

--- analytics/src/analytics_desktop.cc
+++ analytics/src/analytics_desktop.cc
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <future>
 #include <map>
 #include <mutex>
 #include <sstream>
