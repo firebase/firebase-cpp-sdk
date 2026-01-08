ec87d05a7d734931adc86136de5aa3f2961ba877
Austin Benoit <austinbenoit@google.com>
2026-01-08 12:10:10 -0500

    chore: update minimum cmake version to 3.30

--- firestore/integration_test_internal/abseil-cpp.cmake
+++ firestore/integration_test_internal/abseil-cpp.cmake
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-cmake_minimum_required(VERSION 2.8)
+cmake_minimum_required(VERSION 3.30)
 include(ExternalProject)
 
 set(version 20200225)
