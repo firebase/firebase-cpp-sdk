54f73cb4a1a4afe625357f7e370d4bd9c0861605
a-maurice <amaurice@google.com>
2026-02-18 18:30:55 -0800

    Try stripping the ftl result json

--- scripts/gha/read_ftl_test_result.py
+++ scripts/gha/read_ftl_test_result.py
@@ -56,7 +56,7 @@ def main(argv):
   if len(argv) > 1:
     raise app.UsageError("Too many command-line arguments.")
 
-  test_result = json.loads(FLAGS.test_result)
+  test_result = json.loads(FLAGS.test_result.strip("'"))
   tests = []
   for app in test_result.get("apps"):
     app_path = app.get("testapp_path")
