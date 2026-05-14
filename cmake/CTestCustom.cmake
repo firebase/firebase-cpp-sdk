d14c015eebfa9fbb97a68bd9380e75e28a720f60
a-maurice <amaurice@google.com>
2026-05-14 16:34:23 -0700

    Revert "Update CTestCustom.cmake"
    This reverts commit d693778f3b32fd0b46afbfc0745017deea89f96f.

--- cmake/CTestCustom.cmake
+++ cmake/CTestCustom.cmake
@@ -19,7 +19,8 @@ set(CTEST_CUSTOM_TESTS_IGNORE
     uv_test
     uv_test_a
     # Tests from zlib, that can't be disabled normally.
-    zlib
+    example
+    example64
     # Disabling specific firestore tests
     # objc_spec test doesn't fail locally but fails consistently on 
     # github runners
