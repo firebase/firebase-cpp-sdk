f106347ad3a8ce55e5ccd6ce90387c0c84d60b70
AustinBenoit <austinbenoit@google.com>
2026-06-03 13:46:18 -0400

    Change it so that the header files are directly included
    Remove the cmake injection and just include the header files as needed

--- analytics/src/analytics_desktop_dynamic.h
+++ analytics/src/analytics_desktop_dynamic.h
@@ -24,6 +24,7 @@
 // --- Copied from original header ---
 #include <stdbool.h>
 #include <stdint.h>
+#include <stddef.h>
 
 typedef struct GoogleAnalytics_Reserved_Opaque GoogleAnalytics_Reserved;
 
